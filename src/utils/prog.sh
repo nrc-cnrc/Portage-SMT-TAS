f191aa8efc765d2c4d5b7a427a8146626fa25f64
joanise <>
2007-04-11 23:45:36 +0000

    Fix Copyright year.

--- src/utils/prog.sh
+++ src/utils/prog.sh
@@ -7,11 +7,11 @@
 # 
 # COMMENTS:
 #
-# Groupe de technologies langagieres interactives / Interactive Language Technologies Group
+# Technologies langagieres interactives / Interactive Language Technologies
 # Institut de technologie de l'information / Institute for Information Technology
 # Conseil national de recherches Canada / National Research Council Canada
-# Copyright 2006, Sa Majeste la Reine du Chef du Canada /
-# Copyright 2006, Her Majesty in Right of Canada
+# Copyright 2007, Sa Majeste la Reine du Chef du Canada /
+# Copyright 2007, Her Majesty in Right of Canada
 
 echo 'prog.sh, NRC-CNRC, (c) 2006 - 2007, Her Majesty in Right of Canada' >&2
 
