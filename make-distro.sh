9352f3831de7a8f24dcb8125efb9ddd8bc19c551
joanise <>
2007-04-16 19:35:54 +0000

    Add release to release patch handling.

--- make-distro.sh
+++ make-distro.sh
@@ -21,6 +21,8 @@ usage() {
 
 Usage: make-distro.sh [-h(elp)] [-bin] [-nosrc] [-licence PROJECT] [-n]
        [-compile-only] [-compile-host HOST] [-rCVS_TAG|-DCVS_DATE]
+       [-patch-from OLD_CD_DIR:PREREQ_TOKEN
+          [-patch-from OLD_CD_DIR2:PREREQ_TOKEN2 [...]]]
        [-aachen] [-smart-bin] [-smart-src] [-can-univ]
        -dir OUTPUT_DIR
 
@@ -53,6 +55,13 @@ Options:
                 [CanUniv, or BinOnly if -nosrc is specified]
   -n            Not Really: just show what will be done.
   -archive-name Infix to insert in .tar and .iso filenames. []
+  -patch-from   Create patch OLD_CD_DIR_to_OUTPUT_DIR.patch for users who want
+                to patch existing installations instead of doing a fresh
+                install.  May be repeated.  Patches will be included in the
+                .iso file.  PREREQ_TOKEN must be a word that exists in the old
+                INSTALL but in no other distributed versions of INSTALL, past
+                or future.  For v1.0, it should be "2004-2006,", for v1.1 (and
+                subsequent) it should be PORTAGEshared_v1.1 (or vX.Y)
 
 Canned options for specific licensees:
 
@@ -79,6 +88,10 @@ error_exit() {
 }
 
 run_cmd() {
+   if [ "$1" = "-no-error" ]; then
+      shift
+      RUN_CMD_NO_ERROR=1
+   fi
    echo "$*"
    if [ -z "$NOT_REALLY" ]; then
       eval $*
@@ -86,10 +99,11 @@ run_cmd() {
    else
       rc=0
    fi
-   if [ "$rc" != 0 ]; then
+   if [ -z "$RUN_CMD_NO_ERROR" -a "$rc" != 0 ]; then
       echo "Exit status: $rc is not zero - aborting."
       exit 1
    fi
+   RUN_CMD_NO_ERROR=
 }
 
 arg_check() {
@@ -108,6 +122,7 @@ while [ $# -gt 0 ]; do
    -r*|-D*)             VERSION_TAG=$1;;
    -dir)                arg_check 1 $# $1; OUTPUT_DIR=$2; shift;;
    -licence|-license)   arg_check 1 $# $1; LICENCE=$2; shift;;
+   -patch-from)         arg_check 1 $# $1; PATCH_FROM="$PATCH_FROM $2"; shift;;
    -archive-name)       arg_check 1 $# $1; ARCHIVE_NAME=$2; shift;;
    -can-univ)           LICENCE=CanUniv; ARCHIVE_NAME=CanUniv;;
    -smart-src)          LICENCE=SMART; ARCHIVE_NAME=SMART;;
@@ -123,6 +138,25 @@ while [ $# -gt 0 ]; do
    shift
 done
 
+if [ -n "$PATCH_FROM" ]; then
+   for PATCH_ARG in $PATCH_FROM; do
+      OLD_DIR=${PATCH_ARG%:*}
+      PATCHLEVEL_TOKEN=${PATCH_ARG##*:}
+      if [ "$PATCHLEVEL_TOKEN" = "$PATCH_ARG" ]; then
+         error_exit "Invalid -patch-from specification: missing PREREQ_TOKEN"
+      fi
+      if [ ! -f $OLD_DIR/PORTAGEshared/INSTALL ]; then
+         error_exit "Invalid -patch-from specification:" \
+                    "$OLD_DIR/PORTAGEshared/INSTALL doesn't exist"
+      fi
+      if LC_ALL=C egrep -q '(^|[ 	])'$PATCHLEVEL_TOKEN'($|[ 	])' $OLD_DIR/PORTAGEshared/INSTALL; then
+         true
+      else
+         error_exit "Invalid -patch-from specification:" \
+            "$OLD_DIR/PORTAGEshared/INSTALL doesn't contain $PATCHLEVEL_TOKEN"
+      fi
+   done
+fi
 
 do_checkout() {
    if [ -z "$NOT_REALLY" ]; then
@@ -213,7 +247,13 @@ make_iso_and_tar() {
       ARCHIVE_FILE=$VOLID
    fi
    run_cmd pushd ./$OUTPUT_DIR
-      run_cmd mkisofs -V $VOLID -joliet-long -o $ARCHIVE_FILE.iso PORTAGEshared '&>' iso.log
+      if [ -n "$PATCH_FROM" ]; then
+         PATCH_FILES=*_to_*.patch
+      else
+         PATCH_FILES=
+      fi
+      run_cmd mkisofs -V $VOLID -joliet-long -o $ARCHIVE_FILE.iso \
+              PORTAGEshared $PATCH_FILES '&>' iso.log
       run_cmd tar -cvzf $ARCHIVE_FILE.tar.gz PORTAGEshared '>&' tar.log
    run_cmd popd
 }
@@ -221,7 +261,8 @@ make_iso_and_tar() {
 test $OUTPUT_DIR  || error_exit "Missing mandatory -dir argument"
 
 if [ -z "$COMPILE_ONLY" ]; then
-   test $VERSION_TAG || error_exit "Missing mandatory -tag argument"
+   test $VERSION_TAG ||
+      error_exit "Missing mandatory -rCVS_TAG or -DCVS_DATE argument"
 
    do_checkout
    get_user_manual
@@ -251,6 +292,27 @@ if [ -z "$COMPILE_ONLY" ]; then
       run_cmd popd
    else
       run_cmd rm ./$OUTPUT_DIR/PORTAGEshared/doc/code-doc-binonly.html
+   fi
+
+   if [ -n "PATCH_FROM" ]; then
+      for PATCH_ARG in $PATCH_FROM; do
+         OLD_DIR=${PATCH_ARG%:*}
+         PATCHLEVEL_TOKEN=${PATCH_ARG##*:}
+         run_cmd echo Prereq: $PATCHLEVEL_TOKEN \
+                 \> $OUTPUT_DIR/${OLD_DIR}_to_${OUTPUT_DIR}.patch
+         run_cmd -no-error LC_ALL=C TZ=UTC0 diff -Naur \
+                 $OLD_DIR/PORTAGEshared $OUTPUT_DIR/PORTAGEshared \
+                 \>\> $OUTPUT_DIR/${OLD_DIR}_to_${OUTPUT_DIR}.patch
+         if head -2 $OUTPUT_DIR/${OLD_DIR}_to_${OUTPUT_DIR}.patch |
+            grep -q PORTAGEshared/INSTALL
+         then
+            true
+         else
+            echo "WARNING: patch doesn't start with INSTALL,"
+            echo "Prereq: $PATCHLEVEL_TOKEN line will likely not behave as expected."
+            echo "A FIX IS LIKELY REQUIRED TO $0 ITSELF!!!"
+         fi
+      done
    fi
 
    make_iso_and_tar
