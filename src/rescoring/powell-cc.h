8799043ab8ea960607984170f5cce1563d754310
larkins <>
2008-09-29 20:20:16 +0000

    Removing debugging statements from the new powell's code

--- src/rescoring/powell-cc.h
+++ src/rescoring/powell-cc.h
@@ -27,7 +27,7 @@ using namespace boost::numeric;
 namespace Portage
 {
    // This has been modified from the original Numerical Recipes in C++ version in 3 ways:
-   // - The NR implementation of vectors and matrices is replaced by the GSL implementation.
+   // - The NR implementation of vectors and matrices is replaced by the boost implementation.
    // - The original implementation performs a minimization, while this performs a
    //   maximization.
    // - The call to a grid based line-maximization (or -minimization) method is replaced with a
@@ -39,14 +39,6 @@ namespace Portage
       int &iter,
       double &score)
    {
-      /////////////////////////////////////
-      // external calls formerly present here:
-      //  - computeScore(1 arg), defined in powell.h
-      //  - linemax(2 args), a functor call to Powell::linemax, declared in
-      //    powell.h and defined in linemax.h, ultimately resolved to
-      //   LinaMax<Stats>::operator()(2 args).
-      /////////////////////////////////////
-
       // NOTES:
       // score == f_P_N == score(P_i)
 
@@ -54,31 +46,25 @@ namespace Portage
       score = 0.0f;
       iter  = 0;
 
-      const Uint N(p.size());
+      const Uint M(p.size());
 
       double f_0(0.0f);    // Keep track of initial score before exploring all dimensions.
       score = computeScore(p);
       do {
          uVector origine(p);  // Saving the starting point.
          f_0 = score;         // Saving the original starting point's score.
-         cout << "\tf_0: " << f_0 << endl;
 
          // Keep track of the index of the largest decrease.
          double delta_f(0.0f);
          int    best_index(-1);
 
-         for (Uint k(0); k<N; ++k) {
+         for (Uint k(0); k<M; ++k) {
             uVector U_k(uColumn(U, k));
-            cout << "\t\tP_k: " << p << endl;
-            cout << "\t\tU_k: " << U_k << U_k / ublas::norm_inf(U_k) << endl;
             linemax(p, U_k);
-            cout << "\t\tP_k: " << p << endl;
-            cout << "\t\tU_k: " << U_k << U_k / ublas::norm_inf(U_k) << endl;
 
             const double f_P_k     = computeScore(p);
             const double delta_f_k = fabs(f_P_k - score);
             score                  = f_P_k;
-            cout << "\t\tdelta_f_k: " << delta_f_k << endl;
             if (delta_f_k > delta_f) {
                best_index = k;
                delta_f    = delta_f_k;
@@ -86,49 +72,30 @@ namespace Portage
          }
          ++iter;
 
-         cout << "\tP: " << p << endl;
-         cout << "\tU: " << U << endl;
-         cout << "\tdelta_f: " << delta_f << endl;
-         cout << "\tbest_index: " << best_index << endl;
-         //if (2*fabs(f_0 - score) <= ((fabs(f_0) + fabs(score) * tolerance))) break;
-
          // From this point on:
-         //   p == P_N
+         //   p == P_N == final point
          //   score == f_P_N == best_score == f(P_N)
 
          const double f_E = computeScore(2*p - origine);
-         cout << "\tf_E: " << f_E << endl;
-         cout << "\tf_0: " << f_0 << endl;
          // MAXIMIZATION
          if (f_E > f_0) {
             const double f_N = score;  // == f(P_N)
             const double left_hand_side = 2.0f * (-f_0 + 2.0*f_N - f_E) * square(fabs(f_0 - f_N) - delta_f);
             const double right_hand_side = delta_f * square(f_0 - f_E);
-            cout << "\tf_N: " << f_N << endl;
-            cout << "\tleft_hand_side: " << left_hand_side << endl;
-            cout << "\tright_hand_side: " << right_hand_side << endl;
             if (left_hand_side < right_hand_side) {
                assert(best_index >= 0);  // Make sure we have found a best index.
                uVector final_dir = p - origine;
 
+               linemax(p, final_dir);
+               score = computeScore(p);
+
                // Replace the best/biggest decrease direction by the new average direction.
                uColumn U_r(U, best_index);
-               uColumn U_N(U, N-1);
+               uColumn U_N(U, M-1);
                U_r.swap(U_N);
-               U_N = final_dir;// / ublas::norm_inf(final_dir);
-
-               cout << "\t\tp: " << p << endl;
-               cout << "\t\tfinal_dir: " << final_dir << final_dir / ublas::norm_inf(final_dir)  << endl;
-               linemax(p, final_dir);
-               cout << "\t\tp: " << p << endl;
-               cout << "\t\tfinal_dir: " << final_dir << final_dir / ublas::norm_inf(final_dir)  << endl;
-               score = computeScore(p);
-               cout << "\t\tU: " << U << endl;
+               U_N = final_dir;
             }
          }
-
-         cout << "\t" << iter << ", score: " << score << " " << p << endl;  // SAM DEBUG
-         cout << endl;
       } while (2*fabs(f_0 - score) > ((fabs(f_0) + fabs(score) * tolerance)));
    } // ends Powell<ScoreStats>::operator()
 
--- src/rescoring/powell.h
+++ src/rescoring/powell.h
@@ -46,40 +46,51 @@ namespace Portage
          { }
 
          /**
-            Perform's Powell's algorithm, using Och's line maximization algorithm, in order to find parameters
-            p that maximize the score on the training corpus.
-            For the details of Powell's algorithm, see "Numerical Recipes in C", section 10.5.
-            This algorithm attempts to find the parameter vector that will maximize the score, but it is a
-            heuristic, and thus is not guarenteed to find the optimal solution.  It is recommended that
-            the algorithm is run multiple times using different starting points, to improve the likelihood
-            of finding the global maximum.
+            Perform's Powell's algorithm, using Och's line maximization
+            algorithm, in order to find parameters p that maximize the score on
+            the training corpus.
+            For the details of Powell's algorithm, see "Numerical Recipes in
+            C", section 10.5.  This algorithm attempts to find the parameter
+            vector that will maximize the score, but it is a heuristic, and
+            thus is not guarenteed to find the optimal solution.  It is
+            recommended that the algorithm is run multiple times using
+            different starting points, to improve the likelihood of finding the
+            global maximum.
 
             We have the following setup:
-            -   Training foreign sentences f_0, .. , f_{S-1}.
-            -   For each s = 0, .. , S-1, we have candidate translations e_{s,0}, .. , e_{s,K-1} (eg. K-best)
-            -   For each candidate translation, statistics for the score.  These statistics should be additive -
-            the total score for a set of translations is determined by adding up the statistics then computing
-            the score from the sum.  It is assumed that greater allScoreStats are better; if the reverse is true,
-            just use the negative score instead.
-            -   Feature functions h_m(e,f) for m = 0, .. , M-1
+            - Training foreign sentences f_0, .. , f_{S-1}.
+            - For each s = 0, .. , S-1, we have candidate translations
+              e_{s,0}, .. , e_{s,K-1} (eg. K-best)
+            - For each candidate translation, statistics for the score.  These
+              statistics should be additive - the total score for a set of
+              translations is determined by adding up the statistics then
+              computing the score from the sum.  It is assumed that greater
+              allScoreStats are better; if the reverse is true, just use the
+              negative score instead.
+            - Feature functions h_m(e,f) for m = 0, .. , M-1
 
             Parameters to this function is as follows:
-            p  -   A vector containing the starting point in the parameter space.  The "optimal" parameters found
-            will be returned via p.
-            xi -   The directions used in Powell's algorithm.  Generally, the M x M identity is a good starting
-            value for this matrix.  The contents of the matrix will be modified by the algorithm.
-            ftol - The fractional tolerance in the function value, such that when one iteration does not increase
-            the score by at least this much, then the algorithm terminates.
-            TODO: consider whether fractional tolerance is appropriate, or if some other tolerance should
-            be used.
+            p - A vector containing the starting point in the parameter space.
+                The "optimal" parameters found will be returned via p.
+            U - The directions used in Powell's algorithm.  Generally, the M x
+                M identity is a good starting value for this matrix.  The
+                contents of the matrix will be modified by the algorithm.
+            tolerance - The fractional tolerance in the function value, such
+                        that when one iteration does not increase the score by
+                        at least this much, then the algorithm terminates.
+            TODO: consider whether fractional tolerance is appropriate, or if
+                  some other tolerance should be used.
             iter - Will contain the number of iterations done in the algorithm.
-            fret - Will contain the maximum score achieved.
-            H  -   An array of length S, of K x M matrices containing the evaluation of the feature functions at
-            the candidate translations.  Specifically, the (k, m)-th entry of H[s] should contain the
-            value h_m(e_{s,k}, f_s).
-            bleu - An S x K array of score statistics for the candidate sentences.  Suggestively called
-            "bleu", though it is easy to write a class for other types of allScoreStats, so long as the score
-            can be broken into linear components.  The following must be defined for the type ScoreStats:
+            score - Will contain the maximum score achieved.
+            H - An array of length S, of K x M matrices containing the
+                evaluation of the feature functions at the candidate
+                translations.  Specifically, the (k, m)-th entry of H[s] should
+                contain the value h_m(e_{s,k}, f_s).
+            bleu - An S x K array of score statistics for the candidate
+                   sentences.  Suggestively called "bleu", though it is easy to
+                   write a class for other types of allScoreStats, so long as
+                   the score can be broken into linear components.  The
+                   following must be defined for the type ScoreStats:
             double ScoreStats::score()
             Calculates the score (bigger score is better)
             void ScoreStats::output(ostream &out)
@@ -87,28 +98,30 @@ namespace Portage
             ScoreStats operator+(const ScoreStats &s1, const ScoreStats &s2)
             ScoreStats operator-(const ScoreStats &s1, const ScoreStats &s2)
             Produce the sum or difference in statistics.
-            By default, a new object of type ScoreStats must have "zero" statistics - that is, it should
-            represent the score given to an empty set of translations.
-            See bleu.h, bleu.cc (in eval) for an example of the implementation of the BLEU statistics.
-            bleu[s][k] should contain the score statistics assigned to the sentence e_{s,k}.  Of course,
-            the score statistics is not a function only of e_{s,k} but (presumably) of the correct
-            translation as well.
-            S  -   The number of source sentences.
-            K  -   The number of candidate translations for each source sentence.
+            By default, a new object of type ScoreStats must have "zero"
+            statistics - that is, it should represent the score given to an
+            empty set of translations.
+            See bleu.h, bleu.cc (in eval) for an example of the implementation
+            of the BLEU statistics.  bleu[s][k] should contain the score
+            statistics assigned to the sentence e_{s,k}.  Of course, the score
+            statistics is not a function only of e_{s,k} but (presumably) of
+            the correct translation as well.
+            S - The number of source sentences.
+            K - The number of candidate translations for each source sentence.
 
 
-            * @param p     vector containing the starting point in the parameter
-            *              space.
-            * @param xi    directions used in Powell's algorithm.
-            * @param ftol  fractional tolerance
-            * @param iter  returned number of iterations done in the algorithm.
-            * @param fret  returned maximum score achieved.
-            */
+          * @param p     vector containing the starting point in the parameter
+          *              space.
+          * @param U     directions used in Powell's algorithm.
+          * @param tolerance  fractional tolerance
+          * @param iter  returned number of iterations done in the algorithm.
+          * @param score returned maximum score achieved.
+          */
          void operator()(uVector& p,
-               uMatrix& xi,
-               const double ftol,
+               uMatrix& U,
+               const double tolerance,
                int &iter,
-               double &fret);
+               double &score);
 
          private:
             ScoreStats computeStats(const uVector& p);
