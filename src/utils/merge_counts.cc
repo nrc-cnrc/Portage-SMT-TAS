f522d294f7c871a92b17c2191683abb1538651a8
larkins <>
2008-12-02 18:59:00 +0000

    Changing the help message to reflect what this pgm does. Removing all instances of pcre.

--- src/utils/merge_counts.cc
+++ src/utils/merge_counts.cc
@@ -1,7 +1,7 @@
 /**
  * @author Samuel Larkin
  * @file merge_counts.cc
- * @brief Merge sort files by talling counts.
+ * @brief Merge sorted files by talling counts.
  * 
  * 
  * COMMENTS: 
@@ -20,31 +20,23 @@
 #include <string>
 #include <queue>
 #include <iterator>
-//#include <pcre/pcre.h>
 
 using namespace Portage;
 using namespace std;
 
 static char help_message[] = "\n\
-merge_counts [-v][-d][-si]\n\
-   [-p <regex> | -freq | -jpt]\n\
-   [infile [outfile]]\n\
+merge_counts [-v][-d] [outfile [infile]]\n\
 \n\
-Merge count files using a regular expression pattern to extract the count\n\
-from each datum.  The atom in the regular expression represents the count.\n\
+Merge count files where the count is separated by at least one space and is the\n\
+last item on a line.\n\
 \n\
 IMPORTANT:\n\
 - input files must be LC_ALL=C sorted.\n\
-- your custom pattern MUST contain exactly one atom i.e. ()\n\
 \n\
 Options:\n\
 \n\
 -d  Write debugging info. [don't]\n\
 -v  Write progress reports to cerr. [don't]\n\
--si sort input files. [don't]\n\
--p  perl compatible regex with exactly one set of parenthesis.\n\
--freq  merge frequency tables aka [-p \"\t([\\d]+)\"].\n\
--jpt   merge jpt tables aka [-p \"\t([\\d]+)\"].\n\
 ";
 
 // globals
@@ -53,7 +45,6 @@ static bool bDebug = false;
 static bool verbose = false;
 static vector<string> infiles;
 static string outfile("-");
-static string pattern;
 
 static void getArgs(int argc, char* argv[]);
 
@@ -101,19 +92,15 @@ class mergeStream
             iSafeMagicStream   input;  ///< file stream
             Datum*             _data;  ///< Datum
             string             buffer; ///< line buffer
-            //pcre*              expr;   ///< regex to break up the line datum
 
          public:
             /// Default constructor
-            //Stream(const string& file, pcre* expr)
             Stream(const string& file)
             : file(file)
             , input(file)
             , _data(new Datum)
-            //, expr(expr)
             {
                get();
-               //assert(expr != NULL);
             }
 
             ~Stream() { delete _data; }
@@ -126,23 +113,11 @@ class mergeStream
             }
 
             Datum* get() {
-               //int matches[30];
                if (getline(input, buffer)) {
                   const string::size_type pos = buffer.rfind(' ');
                   _data->prefix = buffer.substr(0, pos+1);  // Keep the space
                   if (!convT(buffer.substr(pos).c_str(), _data->count))
                      error(ETWarn, "Count is not a number %s", buffer.c_str());
-                  /*int ret = pcre_exec(expr, NULL, buffer.c_str(), buffer.size(), 0, 0, matches, 30);
-                  if (ret > 0) {
-                     _data->prefix = string(buffer.c_str(), matches[2]);
-                     if (!convT(string(buffer.c_str() + matches[2], matches[3] - matches[2]).c_str(), _data->count))
-                        error(ETWarn, "Count is not a number %s", buffer.c_str());
-                     _data->suffix = string(buffer.c_str() + matches[3]);
-                     return _data;
-                  }
-                  else {
-                     error(ETWarn, "No match on line: %s", buffer.c_str());
-                  }*/
                }
                return NULL;
             }
@@ -166,23 +141,13 @@ class mergeStream
 
       Datum* spare;
       Datum* total;
-      //pcre*  expr;   ///< the master regex
 
    public:
-      mergeStream(const vector<string>& infiles, const string& pattern = "\t([\\d]+)")
+      mergeStream(const vector<string>& infiles)
       : spare(new Datum)
       , total(new Datum)
       {
-         assert(!pattern.empty());
-
-         /*const char *errstr;
-         int erroffset;
-         if (!(expr = pcre_compile(pattern.c_str(), 0, &errstr, &erroffset, 0))) {
-            error(ETFatal, "Cannot make regex from(%s): %s", errstr, pattern.c_str());
-         }*/
-
          for (Uint i(0); i<infiles.size(); ++i) {
-            //queue.push(new Stream(infiles[i], expr));
             queue.push(new Stream(infiles[i]));
          }
 
@@ -238,12 +203,12 @@ int main(int argc, char* argv[])
 {
    getArgs(argc, argv);
 
-   fprintf(stderr, "Attempting to merge using %s:\n", pattern.c_str());
+   fprintf(stderr, "Attempting to merge :\n");
    copy(infiles.begin(), infiles.end(), ostream_iterator<string>(cerr, " "));
    cerr << endl;
 
 
-   mergeStream       ms(infiles, pattern.c_str());
+   mergeStream       ms(infiles);
    oSafeMagicStream  out(outfile);
 
    while (!ms.eof()) {
@@ -257,15 +222,12 @@ int main(int argc, char* argv[])
 
 void getArgs(int argc, char* argv[])
 {
-   const char* switches[] = {"v", "d", "p:", "freq", "jpt"};
+   const char* switches[] = {"v", "d"};
    ArgReader arg_reader(ARRAY_SIZE(switches), switches, 2, -1, help_message);
    arg_reader.read(argc-1, argv+1);
 
    arg_reader.testAndSet("v", verbose);
    arg_reader.testAndSet("d", bDebug);
-   arg_reader.testAndSet("p:", pattern);
-   if (arg_reader.getSwitch("freq")) pattern = "\t([\\d]+)";
-   if (arg_reader.getSwitch("jpt"))  pattern = "\\s+\\|\\|\\|\\s+([\\d]+)\\s*$";
 
    arg_reader.testAndSet(0, "outfile", outfile);
    arg_reader.getVars(1, infiles);
@@ -275,6 +237,4 @@ void getArgs(int argc, char* argv[])
       error(ETFatal, "Invalid output file");
    if (infiles.empty())
       error(ETFatal, "You must provide at least of file to merge");
-   if (pattern.empty())
-      error(ETFatal, "You must provide a pattern or use the preset patterns.");
 }
