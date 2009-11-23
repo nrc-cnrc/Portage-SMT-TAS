3ab16451fa1afba378a1344f80c2fa550ea4616c
joanise <>
2009-11-23 18:28:37 +0000

    Migrated this test suite from Portage.

---
+++ test-suite/unit-testing/filter-distortion-model/run-test.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+# run-test.sh - Unit-test for filter-distortion-model.pl.
+# Tests
+#
+# PROGRAMMER: Samuel Larkin
+#
+# Technologies langagieres interactives / Interactive Language Technologies
+# Inst. de technologie de l'information / Institute for Information Technology
+# Conseil national de recherches Canada / National Research Council Canada
+# Copyright 2009, Sa Majeste la Reine du Chef du Canada /
+# Copyright 2009, Her Majesty in Right of Canada
+
+make all -B
