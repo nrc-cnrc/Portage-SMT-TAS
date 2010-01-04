647f9de627fc473f7cb54d384bb91a5dfbcebf88
joanise <>
2010-01-04 22:02:03 +0000

    Add a clean.sh script here so ../clean-all-test.sh works.

---
+++ test-suite/unit-testing/strip_non_printing/clean.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+# clean.sh - Clean generated files in this test suite.
+# Tests
+#
+# PROGRAMMER: Eric Joanis
+#
+# Technologies langagieres interactives / Interactive Language Technologies
+# Inst. de technologie de l'information / Institute for Information Technology
+# Conseil national de recherches Canada / National Research Council Canada
+# Copyright 2009, Sa Majeste la Reine du Chef du Canada /
+# Copyright 2009, Her Majesty in Right of Canada
+
+# Nothing to clean in this directory.
