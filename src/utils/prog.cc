39b5f456e1474e8a334d79d863863941a288181f
larkins <>
2010-01-19 20:00:45 +0000

    Welcome to 2010.

--- src/utils/prog.cc
+++ src/utils/prog.cc
@@ -8,12 +8,13 @@
  * Technologies langagieres interactives / Interactive Language Technologies
  * Inst. de technologie de l'information / Institute for Information Technology
  * Conseil national de recherches Canada / National Research Council Canada
- * Copyright 2009, Sa Majeste la Reine du Chef du Canada /
- * Copyright 2009, Her Majesty in Right of Canada
+ * Copyright 2010, Sa Majeste la Reine du Chef du Canada /
+ * Copyright 2010, Her Majesty in Right of Canada
  */
+
 #include <iostream>
 #include <fstream>
-#include <file_utils.h>
+#include "file_utils.h"
 #include "arg_reader.h"
 
 using namespace Portage;
