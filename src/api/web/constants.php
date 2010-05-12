55ef94cd56cdbe6a086aa794ae1a85a36267ac97
joanise <>
2010-05-12 18:14:36 +0000

    Remove this obsolete demo code from PORTAGEshared. PortageLive now fully replaces this code with an up-to-date and supported code base.

--- src/api/web/constants.php
+++
@@ -1,9 +0,0 @@
-<?php
-define ("__EN_2_FR_SERVER_HOST__", "leclerc");
-define ("__EN_2_FR_SERVER_PORT_NUM__", "20002");
-define ("__FR_2_EN_SERVER_HOST__", "leclerc");
-define ("__FR_2_EN_SERVER_PORT_NUM__", "20004");
-define ("__CH_2_EN_SERVER_HOST__", "leclerc");
-define ("__CH_2_EN_SERVER_PORT_NUM__", "20006");
-define ("__TRANSLATION_CLIENT_NAME__", "paddle_client");
-?>
