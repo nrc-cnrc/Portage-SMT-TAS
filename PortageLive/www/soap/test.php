f327fc6dbd597cb7608482c9c57fcc2389934652
larkins <>
2010-05-13 15:37:16 +0000

    Adding robustness and easy to understand status message.

--- PortageLive/www/soap/test.php
+++ PortageLive/www/soap/test.php
@@ -1,4 +1,18 @@
 <?php 
-  $client = new SoapClient("PortageLiveAPI.wsdl"); 
-  print "response of getTranslation: " . $client->getTranslation("This is a test."); 
+  print "Translating... This is a test.<BR/>";
+  try {
+    $client = new SoapClient("PortageLiveAPI.wsdl"); 
+    $translation = $client->getTranslation("This is a test.");
+    if ($translation != "") {
+      print "<H1><B><FONT COLOR=\"#00FF00\">Successful</FONT></B></H1>";
+      print "response of getTranslation: " . $translation; 
+    }
+    else {
+      print "<FONT COLOR=\"#00FF00\"><BR/><H1><B>Error</B></H1></FONT>";
+    }
+  }
+  catch (SoapFault $exception) {
+    print "SOAP Fault: (faultcode: {$fault->faultcode}, faultstring: {$fault->faultstring})" . "<BR/>";
+    var_dump($soapFault);
+  }
 ?> 
