19cf50c83346c7455b1c6f26fa190605d1647fdd
larkins <>
2010-05-20 19:59:39 +0000

    Adding NRC's header & footer.

--- PortageLive/www/soap/test.php
+++ PortageLive/www/soap/test.php
@@ -1,3 +1,14 @@
+<!DOCTYPE html
+	PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN"
+	 "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd"> 
+<html xmlns="http://www.w3.org/1999/xhtml" lang="en-US" xml:lang="en-US"> 
+<head> 
+<title>PORTAGELive</title> 
+<meta http-equiv="Content-Type" content="text/html; charset=iso-8859-1" /> 
+</head> 
+<body>
+<p align="center"><img src="/images/NRC_banner_e.jpg" /></p>
+
 <?php 
   print "Translating... This is a test.<BR/>";
   try {
@@ -16,3 +27,30 @@
     var_dump($soapFault);
   }
 ?> 
+
+<hr/>
+<table width="100%" cellspacing="0" cellpadding="0" border="0">
+   <tr>
+      <td width="20%" align="right" valign="bottom">
+	 <img alt="NRC-IIT - Institute for Information Technology" src="/images/iit_sidenav_graphictop_e.gif" height="54" />
+      </td>
+      <td width="60%" align="center" valign="bottom">
+	 <img width="286" alt="National Research Council Canada" src="/images/mainf1.gif" height="44" />
+      </td> 
+      <td width="20%" align="left" valign="center">
+	 <img width="93" alt="Government of Canada" src="/images/mainWordmark.gif" height="44" />
+      </td>
+   </tr> 
+   <tr>
+      <td align="right" valign="top">
+	 <img alt="NRC-IIT - Institute for Information Technology" src="/images/iit_sidenav_graphicbottom_e.gif" />
+      </td> 
+      <td align="center" valign="top">
+	 <small>Technologies langagi&egrave;res interactives / Interactive Language Technologies <br /> Institut de technologie de l'information / Institute for Information Technology <br /> Conseil national de recherches Canada / National Research Council Canada <br /> Copyright 2004&ndash;2010, Sa Majest&eacute; la Reine du Chef du Canada /  Her Majesty in Right of Canada <br /> <a href="/portage_notices.html">Third party Copyright notices</a>
+	 </small>
+      </td>
+   </tr>
+</table>
+
+</body> 
+</html>
