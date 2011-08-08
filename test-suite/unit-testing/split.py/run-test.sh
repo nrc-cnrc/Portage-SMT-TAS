9b5f0d68e3fbf4d2e726ba7575b1b30c2ebfaa66
larkins <>
2011-08-08 17:10:55 +0000

    Initial version.

---
+++ test-suite/unit-testing/split.py/run-test.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+# run-test.sh - Unit-test for psplit.py
+# Tests
+#
+# PROGRAMMER: Samuel Larkin
+#
+# Technologies langagieres interactives / Interactive Language Technologies
+# Inst. de technologie de l'information / Institute for Information Technology
+# Conseil national de recherches Canada / National Research Council Canada
+# Copyright 2009, Sa Majeste la Reine du Chef du Canada /
+# Copyright 2009, Her Majesty in Right of Canada
+
+\make clean
+\make all -B
+
+exit
