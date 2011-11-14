b330af3961084fb8b184871304cc2644c49ee4ed
stewartd <>
2011-11-14 19:57:33 +0000

    Necessary pieces for building an rpm containing Python 2.7.1 for inclusion in a PortageLive VM.

---
+++ PortageLive/python/control.spec
@@ -0,0 +1,20 @@
+Summary: This package contains all required files required for python.
+Name: PortageLive-python
+Version: 2.7.1
+Release: 0
+License: Copyright © 2004-2011 Her Majesty in Right of Canada. see { commercial, research, evaluation } LICENCE
+Vendor: National Research Council Canada
+Group: translation
+URL: http://www.nrc-cnrc.gc.ca/
+BuildArchitectures: x86_64
+Requires: php php-soap
+
+%description
+Included in this package are the files for a python 2.7.1 installation.
+
+#
+# Do not put anything below the %defattr line; the list of files
+# in this package are automatically written there.
+#
+%files
+%defattr(-,root,root)
---
+++ PortageLive/python/prep-file-layout.sh
@@ -0,0 +1,84 @@
+#!/bin/bash
+# $Id$
+# @file prep-file-layoyt.sh
+# @brief Put the Python into the installed structure for portageLive
+# 
+# @author Darlene Stewart, based on other portageLive prep-file-layoyt.sh scripts
+# 
+# Technologies langagieres interactives / Interactive Language Technologies
+# Inst. de technologie de l'information / Institute for Information Technology
+# Conseil national de recherches Canada / National Research Council Canada
+# Copyright 2011, Sa Majeste la Reine du Chef du Canada /
+# Copyright 2011, Her Majesty in Right of Canada
+
+usage() {
+   for msg in "$@"; do
+      echo $msg >&2
+   done
+   cat <<==EOF== >&2
+
+Usage: ./prep-file-layout.sh
+
+  Copy the python 2.7 files into the structure for packaging into an RPM or
+  installing manually on a translation server.
+
+  The python installation is put in /opt/python-2.7.1 and 
+  /opt/Portage/bin/python is linked to the python /opt/python-2.7.1/bin/pyton.
+
+  Once the layout has been prepared, you can package it into an RPM using
+  ../scripts/make-rpm.sh
+
+==EOF==
+
+   exit 1
+}
+
+arg_check() {
+   if [ $2 -le $1 ]; then
+      usage "Missing argument to $3 option."
+   fi
+}
+
+set -o errexit
+
+FIXED_IP=
+while [ $# -gt 0 ]; do
+   case "$1" in
+   -h|-help)            usage;;
+   --)                  shift; break;;
+   -*)                  error_exit "Unknown option $1.";;
+   *)                   break;;
+   esac
+   shift
+done
+[[ $# -gt 0 ]] && usage "Superfluous argument(s) $*"
+
+PYTHON_DIR=python-2.7.1-install
+
+PYTHON_VERSION=$(basename ${PYTHON_DIR})
+PYTHON_VERSION=${PYTHON_VERSION/%-install}
+
+DEST=rpm.build.root/opt/${PYTHON_VERSION}
+
+# Create directory structure
+mkdir -p ${DEST}
+mkdir -p rpm.build.root/opt/Portage/bin
+
+if [[ $PYTHON_DIR =~ : ]]; then
+   CP_CMD="rsync -avrzlp"
+else
+   CP_CMD="cp -p -r"
+fi
+
+# Copy the python directories
+${CP_CMD} ${PYTHON_DIR}/opt/${PYTHON_VERSION}/* $DEST
+
+
+# Create the link in /opt/Portage/bin
+ln -s /opt/${PYTHON_VERSION}/bin/python rpm.build.root/opt/Portage/bin/
+
+
+# Set proper permissions on the directory and file structure
+find rpm.build.root -type d | xargs chmod 755
+find rpm.build.root -type f | xargs chmod u+rw,g+r,g-w,o+r,o-w
+
