40b72827fd7c6f5d41e5f85c7aa5086851dcf9cb
joanise <>
2012-06-04 17:01:33 +0000

    Argh, not ready to commit this!  Unit testing has to be adjusted too...

--- src/tm/phrase_table_writer.h
+++ src/tm/phrase_table_writer.h
@@ -83,7 +83,7 @@ bool dumpMultiProb(ostream& ofs, Uint left_lang,
    // EJJ High precision is fine and all, but costs us in space in CPT files,
    // as well as in TPPT files, where the encoding is more expensive to keep
    // all those pointless bits around.
-   //ofs.precision(9); // Enough to keep all the precision of a float
+   ofs.precision(9); // Enough to keep all the precision of a float
    string p1, p2;
    vector<double> vals;
 
