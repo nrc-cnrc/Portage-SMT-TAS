6a2942b32aca7312a5f0c5622888672702659fdb
joanise <>
2012-07-09 19:31:40 +0000

    Sweep through installation instructions.

--- SETUP.bash
+++ SETUP.bash
@@ -21,9 +21,9 @@ PORTAGE=$HOME/Portage1.5.0
 # Extra dynamic libraries.  If you had to install dynamic libraries in custom
 # locations to get Portage 1.5 running, list the PATHs where the .so files
 # are located in this variable.  Multiple directories should be separated by
-# colons.  The sample value shown here would apply if you installed g++ 4.2.0
-# in /opt/gcc-4.2.0 on a 64 bit machine.
-#EXTRA_DYNLIB_PATH=/opt/gcc-4.2.0/lib64:/opt/gcc-4.2.0/lib
+# colons.  The sample value shown here would apply if you installed g++ 4.6.0
+# in /opt/gcc-4.6.0 on a 64 bit machine.
+#EXTRA_DYNLIB_PATH=/opt/gcc-4.6.0/lib64:/opt/gcc-4.6.0/lib
 
 # Extra program PATHs.  If you installed Python, Perl, your language modelling
 # toolkit, or other required programs in non-standard locations, you can add
