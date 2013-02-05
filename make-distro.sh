4b6244d9d4e63b65ff55706c04a77cbe702257c4
Eric Joanis <Eric.Joanis@cnrc-nrc.gc.ca>
2013-02-05 14:38:07 -0500

    Remove unecessary LICENCE files and the resulting silly distro variants.

--- make-distro.sh
+++ make-distro.sh
@@ -20,11 +20,11 @@ usage() {
    done
    cat <<==EOF==
 
-Usage: make-distro.sh [-h(elp)] [-bin] [-nosrc] [-licence PROJECT] [-n]
+Usage: make-distro.sh [-h(elp)] [-bin] [-nosrc] [-n]
        [-compile-only] [-compile-host HOST] [-r GIT_TAG]
        [-patch-from OLD_CD_DIR:PREREQ_TOKEN
           [-patch-from OLD_CD_DIR2:PREREQ_TOKEN2 [...]]]
-       [-aachen] [-smart-bin] [-smart-src] [-can-univ] [-can-biz]
+       [-src] [-bin2013]
        [-d GIT_PATH] [-framework FRAMEWORK]
        -dir OUTPUT_DIR
 
@@ -62,9 +62,6 @@ Options:
   -compile-host with -bin, compile locally and also log on to HOST to compile
                 again - must be on the same file system as the local machine.
   -nosrc        do not include src code [do]
-  -licence      Copy the LICENCE file for PROJECT.  Can be CanUniv, SMART,
-		Altera, CanBiz, or BinOnly (which means no LICENCE* file is
-		copied).  [CanUniv, or BinOnly if -nosrc is specified]
   -n            Not Really: just show what will be done.
   -no-doxy      Don't generate the doxy files (use for faster testing) [do]
   -no-usage     Don't generate the usage web page [do]
@@ -87,14 +84,8 @@ Canned options for specific licensees:
   These options chooose the right combinations of the above for specific
   licensees.  -dir and -r must still be specified.
 
-  -can-univ     Same as: -licence CanUniv -archive-name CanUniv
-  -aachen       Same as: -bin -nosrc -licence BinOnly -compile-host leclerc
-                         -archive-name BinOnly
-  -smart-bin    Same as: -aachen
-  -bin2010      Same as: -bin -nosrc -licence BinOnly -archive-name BinOnly
-  -smart-src    Same as: -licence SMART -archive-name SMART
-  -altera       Same as: -licence ALTERA -archive-name Altera
-  -can-biz	Same as: -licence CanBiz -archive-name CanBiz
+  -src          Same as: -archive-name src
+  -bin2013      Same as: -bin -nosrc -archive-name bin
 
 Distro creation check list:
   - Change the version number and year in src/utils/portage_info.cc,
@@ -164,17 +155,10 @@ while [ $# -gt 0 ]; do
    -nosrc)              NO_SOURCE=1;;
    -r)                  arg_check 1 $# $1; VERSION_TAG="$2"; shift;;
    -dir)                arg_check 1 $# $1; OUTPUT_DIR=$2; shift;;
-   -licence|-license)   arg_check 1 $# $1; LICENCE=$2; shift;;
    -patch-from)         arg_check 1 $# $1; PATCH_FROM="$PATCH_FROM $2"; shift;;
    -archive-name)       arg_check 1 $# $1; ARCHIVE_NAME=$2; shift;;
-   -can-univ)           LICENCE=CanUniv; ARCHIVE_NAME=CanUniv;;
-   -smart-src)          LICENCE=SMART; ARCHIVE_NAME=SMART;;
-   -aachen|-smart-bin)  INCLUDE_BIN=1; NO_SOURCE=1; LICENCE=BinOnly
-                        COMPILE_HOST=leclerc; ARCHIVE_NAME=BinOnly;;
-   -bin2010)            INCLUDE_BIN=1; NO_SOURCE=1; LICENCE=bin2010
-                        ARCHIVE_NAME=BinOnly;;
-   -altera)             LICENCE=ALTERA; ARCHIVE_NAME=Altera;;
-   -can-biz)		LICENCE=CanBiz; ARCHIVE_NAME=CanBiz;;
+   -src)                ARCHIVE_NAME=src;;
+   -bin2013)            INCLUDE_BIN=1; NO_SOURCE=1; ARCHIVE_NAME=bin;;
    -framework)          arg_check 1 $# $1; FRAMEWORK=$2; shift;;
    -v|-verbose)         VERBOSE=$(( $VERBOSE + 1 ));;
    -debug)              DEBUG=1;;
@@ -250,29 +234,6 @@ do_checkout() {
       fi
       run_cmd 'find PORTAGEshared -name .git\* | xargs rm -rf'
       run_cmd 'rm PORTAGEshared/.[a-z]*'
-
-      if [ "$LICENCE" = SMART ]; then
-         echo Keeping only SMART licence info.
-         run_cmd find PORTAGEshared -maxdepth 1 -name LICENCE\* \| \
-                 grep -v -x PORTAGEshared/LICENCE_SMART \| xargs rm -f
-      elif [ "$LICENCE" = CanUniv -o \( -z "$LICENCE" -a -z "$NO_SOURCE" \) ]; then
-         echo Keeping only Canadian University licence info.
-         run_cmd find PORTAGEshared -maxdepth 1 -name LICENCE\* \| \
-                 grep -v -x PORTAGEshared/LICENCE \| xargs rm -f
-      elif [ "$LICENCE" = BinOnly -o -n "$NO_SOURCE" ]; then
-         echo No source: removing all LICENCE files.
-         run_cmd find PORTAGEshared -maxdepth 1 -name LICENCE\* \| xargs rm -f
-      elif [ "$LICENCE" = ALTERA ]; then
-         echo Keeping only the Altera licence info.
-         run_cmd find PORTAGEshared -maxdepth 1 -name LICENCE\* \| \
-                 grep -v -x PORTAGEshared/LICENCE_ALTERA \| xargs rm -f
-      elif [ "$LICENCE" = CanBiz ]; then
-         echo Keeping only the Canadian companies licence info.
-         run_cmd find PORTAGEshared -maxdepth 1 -name LICENCE\* \| \
-                 grep -v -x PORTAGEshared/LICENCE_COMPANY \| xargs rm -f
-      else
-         error_exit "Invalid -licence specification"
-      fi
 
       echo Removing -Werror from src/build/Makefile.incl.
       if [[ ! $NOT_REALLY ]]; then
@@ -416,13 +377,13 @@ make_iso_and_tar() {
    echo Generating tar ball and iso file.
    VERSION="${VERSION_TAG#-r}"
    VERSION="${VERSION// /.}"
-   VOLID=Portage_${VERSION}
+   VOLID=${VERSION}
    VOLID=`echo "$VOLID" | perl -pe 's#[/:]#.#g'`
    #echo $VOLID
-   ISO_VOLID=Portage`echo $VERSION | sed -e 's/v//g' -e 's/_/./g'`
+   ISO_VOLID=$VERSION
    ISO_VOLID=${ISO_VOLID:0:31}
    if [ -n "$ARCHIVE_NAME" ]; then
-      ARCHIVE_FILE=${VOLID}_${ARCHIVE_NAME}
+      ARCHIVE_FILE=${VOLID}-${ARCHIVE_NAME}
    else
       ARCHIVE_FILE=$VOLID
    fi
