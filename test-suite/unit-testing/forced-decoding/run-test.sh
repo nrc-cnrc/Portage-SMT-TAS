15fc3fe4d77627590f1562a953bb7d5990241368
Eric Joanis <Eric.Joanis@cnrc-nrc.gc.ca>
2013-11-01 16:31:18 -0400

    Fix forced-decoding unit test suite.
    1- needs 2 CUs, because one of the cases takes around 7 GB of RAM: use
       run-parallel.sh to make sure jobs are scheduled with enough RAM.
    
    2- the emptyjpt case passed spuriously: it fail complaining about the jpt being
       empty, not complaining about canoe.ini not being found!

--- test-suite/unit-testing/forced-decoding/run-test.sh
+++ test-suite/unit-testing/forced-decoding/run-test.sh
@@ -1,4 +1,11 @@
 #!/bin/bash
 
 make clean
-make all -B
+if [[ $PBS_JOBID ]]; then
+   # When running in a PBS job, we need at least 2 workers.  run-parallel.sh
+   # will automatically submit a subjob if it's not already running in a two-CU
+   # job, but won't otherwise.
+   run-parallel.sh -psub -2 -e 'make all -B' 1
+else
+   make all -B
+fi
