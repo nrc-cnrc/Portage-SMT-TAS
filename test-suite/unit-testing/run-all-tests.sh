39bec39289afb3fdb8458fd1e42ccc12d846f006
Eric Joanis <Eric.Joanis@cnrc-nrc.gc.ca>
2015-04-16 15:01:05 -0400

    Starting unit test sweep with all improvements to run-all-tests.sh

--- test-suite/unit-testing/run-all-tests.sh
+++ test-suite/unit-testing/run-all-tests.sh
@@ -9,26 +9,28 @@
 # Copyright 2008, Sa Majeste la Reine du Chef du Canada /
 # Copyright 2008, Her Majesty in Right of Canada
 
-echo 'run-all-tests.sh, NRC-CNRC, (c) 2008-2010, Her Majesty in Right of Canada' 1>&2
-
-if [[ "$1" == "-j" ]]; then
-   PARALLEL_MODE=1
-   PARALLEL_LEVEL=$2
-   shift
-   shift
-fi
-
-if [[ "$1" =~ "^-" ]]; then
+usage() {
    echo "Usage: $0 [test-suite [test-suite2 [...]]]
    Run the specified test suites, or all test suites if none are specified.
    Each test suite must contain a script named run-test.sh which returns 0
    as exit status if the suite passes, non-zero otherwise.
 
 Option:
-   -j N   Run the tests N-ways parallel.
-"
+   -j N      Run the tests N-ways parallel [1]
+   -local L  Run L parallel workers locally [calculated by run-parallel.sh]
+" >&2
    exit
-fi
+}
+
+while [ $# -gt 0 ]; do
+   case "$1" in
+   -j)      PARALLEL_MODE=1; PARALLEL_LEVEL=$2; shift;;
+   -local)  LOCAL="-local $2"; shift;;
+   -*)      usage;;
+   *)       break;;
+   esac
+   shift
+done
 
 TEST_SUITES=$*
 if [[ ! $TEST_SUITES ]]; then
@@ -38,23 +40,18 @@ fi
 echo ""
 echo Test suites to run: $TEST_SUITES
 
-# highlight the text by putting it in red
-highlight() {
-   egrep --line-buffered --color '.*\*.*|$'
-}
-
 if [[ $PARALLEL_MODE ]]; then
    LOG=.log.run-all-tests-parallel
    PARALLEL_MODE=
    for suite in $TEST_SUITES; do
       echo $0 $suite
    done |
-      run-parallel.sh -unordered-cat -v - $PARALLEL_LEVEL 2>&1 |
+      run-parallel.sh -on-error continue $LOCAL -unordered-cat - $PARALLEL_LEVEL 2>&1 |
       tee $LOG |
       grep --line-buffered '^\[' |
-      highlight
+      egrep --line-buffered --color '.*\*.*|$'
    grep PASSED $LOG | grep -v 'test suites' | sort -u
-   grep FAILED $LOG | grep -v 'test suites' | sort -u | highlight
+   grep FAILED $LOG | grep -v 'test suites' | sort -u
 
    if grep -q FAILED $LOG; then
       exit 1
@@ -75,18 +72,18 @@ for TEST_SUITE in $TEST_SUITES; do
    echo Running $TEST_SUITE
    if cd -- $TEST_SUITE; then
       if [[ ! -x ./run-test.sh ]]; then
-         echo '***' FAILED $TEST_SUITE: can\'t find or execute ./run-test.sh | highlight
+         echo '***' FAILED $TEST_SUITE: can\'t find or execute ./run-test.sh
          FAIL="$FAIL $TEST_SUITE"
       elif run_test; then
          echo PASSED $TEST_SUITE
       else
-         echo '***' FAILED $TEST_SUITE: ./run-test.sh returned $? | highlight
+         echo '***' FAILED $TEST_SUITE: ./run-test.sh returned $?
          FAIL="$FAIL $TEST_SUITE"
       fi
 
       cd ..
    else
-      echo '***' FAILED $TEST_SUITE: could not cd into $TEST_SUITE | highlight
+      echo '***' FAILED $TEST_SUITE: could not cd into $TEST_SUITE
       FAIL="$FAIL $TEST_SUITE"
    fi
 done
@@ -94,7 +91,7 @@ done
 echo ""
 echo =======================================
 if [[ $FAIL ]]; then
-   echo '***' FAILED these test suites:$FAIL | highlight
+   echo '***' FAILED these test suites:$FAIL
    exit 1
 else
    echo PASSED all test suites
