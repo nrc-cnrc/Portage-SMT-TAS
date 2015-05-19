2b3ebd30dbd1041cf4b85b49c787dc1523632a9f
Eric Joanis <Eric.Joanis@cnrc-nrc.gc.ca>
2015-05-19 10:40:50 -0400

    Added coarse-tm test suite from R&D

---
+++ test-suite/unit-testing/coarse-tm/run-test.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+# @file run-test.sh
+# @brief Run this test suite, with a non-zero exit status if it fails
+#
+# @author Eric Joanis
+#
+# Technologies langagieres interactives / Interactive Language Technologies
+# Inst. de technologie de l'information / Institute for Information Technology
+# Conseil national de recherches Canada / National Research Council Canada
+# Copyright 2012, Sa Majeste la Reine du Chef du Canada /
+# Copyright 2012, Her Majesty in Right of Canada
+
+make clean
+make all
+
+exit
