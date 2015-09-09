826d23a7d211fe7eefed465355201c023118e968
Eric Joanis <Eric.Joanis@cnrc-nrc.gc.ca>
2015-09-09 10:39:04 -0400

    www/prep-file-layout.sh now asks for one of -dynamic-ip or -ip.
    Rationale: this is better than defaulting to the seldom use dynamic setting

--- PortageLive/www/prep-file-layout.sh
+++ PortageLive/www/prep-file-layout.sh
@@ -17,7 +17,7 @@ usage() {
    done
    cat <<==EOF== >&2
 
-Usage: ./prep-file-layout.sh [-ip FIXED_IP_ADDRESS]
+Usage: ./prep-file-layout.sh [-dynamic-ip | -ip STATIC_IP_ADDRESS]
 
   Copy the CGI and SOAP files into the structure for packaging into an RPM or
   installing manually on a translation server.
@@ -26,19 +26,35 @@ Usage: ./prep-file-layout.sh [-ip FIXED_IP_ADDRESS]
   and are then copied to the web server structure at each boot of the Virtual
   Appliance, with the IP address inserted in the right place.
 
-  If you specify -ip FIXED_IP_ADDRESS, the SOAP files will be placed in their
+  If you specify -ip STATIC_IP_ADDRESS, the SOAP files will be placed in their
   proper installed location, with the IP address hard-coded.  Only use this
   method for physical machines with static IPs.
 
   Once the layout has been prepared, you can package it into an RPM using
   ../scripts/make-rpm.sh
 
-Option:
+Arguments (exactly one of these two is required):
 
-  -ip  Hard code the IP address for the translation server.  [Don't]
+  -ip           Hard code static the IP address for the translation server.
+  -dynamic-ip   Prepare files for a server with a dynamic IP address.
 
+Options:
+
+  -h(elp)       Display this help message.
 ==EOF==
 
+   exit 1
+}
+
+error_exit() {
+   {
+      PROG_NAME=`basename $0`
+      echo -n "$PROG_NAME fatal error: "
+      for msg in "$@"; do
+         echo $msg
+      done
+      echo "Use -h for help."
+   } >&2
    exit 1
 }
 
@@ -51,9 +67,11 @@ arg_check() {
 set -o errexit
 
 FIXED_IP=
+DYNAMIC_IP=
 while [ $# -gt 0 ]; do
    case "$1" in
    -ip)                 arg_check 1 $# $1; FIXED_IP=$2; shift;;
+   -dynamic-ip)         DYNAMIC_IP=1;;
    -h|-help)            usage;;
    --)                  shift; break;;
    -*)                  error_exit "Unknown option $1.";;
@@ -70,8 +88,12 @@ HTML_DIR="${ROOT_DIR}/var/www/html"
 # using DHCP or a VM.
 if [[ $FIXED_IP ]]; then
    SOAP_DEST=${HTML_DIR}
-else
+elif [[ $DYNAMIC_IP ]]; then
    SOAP_DEST=${ROOT_DIR}/opt/PortageII/www
+else
+   error_exit "Please provide your server\'s static IP address via -ip," \
+      "or use -dynamic-ip to enable the dynamic IP mechanism" \
+      "(in that case, don't forget the subsequent-boot script in ../va/)."
 fi
 
 # Create directory structure
