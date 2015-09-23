b1e086215b4d53d090367d04e97a2ca78ffcd1df
Samuel Larkin <Samuel.Larkin@cnrc-nrc.gc.ca>
2015-09-23 16:08:01 -0400

    Initial version of prime.sh's unittests.

---
+++ test-suite/unit-testing/prime.sh/run-test.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+# @file run-test.sh
+# @brief Run this testSuite.
+#
+# @author Samuel Larkin
+#
+# Technologies langagieres interactives / Interactive Language Technologies
+# Inst. de technologie de l'information / Institute for Information Technology
+# Conseil national de recherches Canada / National Research Council Canada
+# Copyright 2015, Sa Majeste la Reine du Chef du Canada /
+# Copyright 2015, Her Majesty in Right of Canada
+
+
+make clean
+make
+exit
