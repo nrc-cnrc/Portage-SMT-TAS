8a503d2ff78ccb7400daa50f44267fbacf7de461
Eric Joanis <Eric.Joanis@cnrc-nrc.gc.ca>
2016-01-15 17:40:51 -0500

    forced-decoding test suite: add support for running it on the GPSC

--- test-suite/unit-testing/forced-decoding/run-test.sh
+++ test-suite/unit-testing/forced-decoding/run-test.sh
@@ -6,6 +6,10 @@ if [[ $PBS_JOBID ]]; then
    # will automatically submit a subjob if it's not already running in a two-CU
    # job, but won't otherwise.
    run-parallel.sh -psub -2 -e 'make all -B' 1
+elif [[ $GECOSHEP_JOB_ID ]]; then
+   # What we really need is just more memory, so we ask for that instead of a cluster that
+   # supports it.
+   run-parallel.sh -psub "-mem 9" -e 'make all -B' 1
 else
    make all -B
 fi
