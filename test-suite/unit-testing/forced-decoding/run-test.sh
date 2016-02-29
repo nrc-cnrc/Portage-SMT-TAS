611347215f953bcf15594f43917b7ca3cceb0c95
Eric Joanis <Eric.Joanis@cnrc-nrc.gc.ca>
2016-02-29 13:46:00 -0500

    forced-decoding test suite: run-parallel.sh -psub "-mem 8" now works everywhere
    We no longer need to detect which cluster we're on in launching this test suite: in
    non-cluster mode, run-parallel just runs the command locally; in cluster mode, -mem 8 will
    always make sure we have enough memory to run the test suite, using subjobs when
    necessary.

--- test-suite/unit-testing/forced-decoding/run-test.sh
+++ test-suite/unit-testing/forced-decoding/run-test.sh
@@ -1,15 +1,4 @@
 #!/bin/bash
 
 make clean
-if [[ $PBS_JOBID ]]; then
-   # When running in a PBS job, we need at least 2 workers.  run-parallel.sh
-   # will automatically submit a subjob if it's not already running in a two-CU
-   # job, but won't otherwise.
-   run-parallel.sh -psub -2 -e 'make all -B' 1
-elif [[ $GECOSHEP_JOB_ID ]]; then
-   # What we really need is just more memory, so we ask for that instead of a cluster that
-   # supports it.
-   run-parallel.sh -psub "-mem 9" -e 'make all -B' 1
-else
-   make all -B
-fi
+run-parallel.sh -psub "-mem 8" -e 'make all -B' 1
