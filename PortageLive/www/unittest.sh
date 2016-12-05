a8b2b47a97dc6527b511d315fa0ed4347c7debf8
Samuel Larkin <Samuel.Larkin@cnrc-nrc.gc.ca>
2016-12-05 16:35:47 -0500

    PortageLiveLib::incrementalTrainingAddSentencePair() Unittest

---
+++ PortageLive/www/unittest.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+mkdir -p plive
+
+# Command line Unittest where no server is required.
+php $PHPUNIT_HOME/phpunit-4.8.phar  --colors=always  tests/testIncrementalTrainingAddSentencePair.php
