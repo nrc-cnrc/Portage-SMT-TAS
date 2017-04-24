da7d8da7d16298b8ff8ddf348073486770e7c919
Eric Joanis <Eric.Joanis@cnrc-nrc.gc.ca>
2017-04-24 10:21:09 -0400

    count_unal_words: optimize to reduce unecessary memory bloat; now faster and tiny mem footprint.
    The high-level iterator interface in PhraseTableUint it really nice, but one
    side effect is that it builds a vocabulary over phrases in each language, which
    can be expensive. count_unal_words never uses that voc, so go lower level and
    thus make it run in less than 1 GB for almost in input size.
    
    (cherry picked from Portage R&D commit 30e01069a9e26fa5e31a9753ffb08a5733150904)

--- src/tm/count_unal_words.cc
+++ src/tm/count_unal_words.cc
@@ -47,7 +47,7 @@ void writeContextPhrase(const string& word, const PhraseTableUint::iterator& p,
 {
    static string src, tgt;
    cerr << word << " - unaligned in: "
-        << p.getPhrase(1, src) << " ||| " << p.getPhrase(2, src) << " ||| "
+        << p.getPhrase(1, src) << " ||| " << p.getPhrase(2, tgt) << " ||| "
         << p.getJointFreq() << " " << al << endl;
 }
 
@@ -60,19 +60,71 @@ int main(int argc, char* argv[])
    CountingVoc voc;
 
    for (Uint i = 0; i < files.size(); ++i) {
-      PhraseTableUint pt;
-      pt.readJointTable(files[i], true);
-      
-      string src, tgt, al;
+      string al;
       vector<string> stoks, ttoks;
       vector< vector<Uint> > sets;
       vector<bool> tpos;
+
+      iSafeMagicStream in(files[i]);
+      string line;
+
+      Voc alignment_voc;
+      Uint lineno = 0;
+      while (getline(in, line)) {
+         ++lineno;
+         vector<string> toks;
+         PhraseTableUint::ToksIter b1, e1, b2, e2, v, a, f;
+         PhraseTableBase::extractTokens(line, toks, b1, e1, b2, e2, v, a, f);
+         stoks.assign(b1,e1);
+         ttoks.assign(b2,e2);
+         Uint jointFreq = 0;
+         if (!conv(*v, jointFreq))
+            error(ETFatal, "Cannot convert count %s to an integer on line %d of phrase table %s",
+                  v->c_str(), lineno, files[i].c_str());
+         if (a != f) {
+            assert((*a)[0] == 'a');
+            string alignments = a->substr(2);
+            AlignmentFreqs<Uint> alignment_freqs;
+            parseAndTallyAlignments(alignment_freqs, alignment_voc, alignments);
+            displayAlignments(al, alignment_freqs, alignment_voc, stoks.size(), ttoks.size(), false, true);
+         } else {
+            al.clear();
+         }
+
+      /*
+      // EJJ APR2017 - This original code was more elegant, taking advantage of
+      // the File iterator strategy implemented for PhraseTableUint, which
+      // means not loading the whole phrase table while having the nice,
+      // easy-to-use iterator interface. Unfortunately, the iterator stills
+      // builds a vocabulary over phrases in each language, and that ends up
+      // requiring lots of memory, for something that is not needed in this
+      // program. A clean solution would be to modify FileIteratorStrategy so
+      // that it looks up id1 and id2 only when needed, i.e., when
+      // iterator::getPhraseIndex() gets called, instead of in operator++(),
+      // but that would require a pretty significant rewrite of the
+      // IteratorStrategy class hierarchy, and I'm not up for that task right
+      // now.  So instead, I resort to using the lower-level extractTokens()
+      // directly.
+      //
+      // Results:
+      // On a small Hansard JPT with 23M phrase pairs, we go from 130s to 72s
+      // runtime, and from 1.0GB to .01GB RAM.
+      // On a WMT-Russian JPT with 419M phrase pairs, we go from 49 to 32 min
+      // runtime, but, more importantly, from 9.2GB to 0.2GB RAM.
+      // Much larger JPTs sometimes required many 10s of GBs, now their
+      // footprint will only be dictated by the word-vocabulary size, so
+      // typically less than 1GB, even for gigantic systems.
+
+      //PhraseTableUint pt;
+      //pt.readJointTable(files[i], true);
 
       for (PhraseTableUint::iterator p = pt.begin(); p != pt.end(); ++p) {
          
          p.getPhrase(1, stoks);
          p.getPhrase(2, ttoks);
          p.getAlignmentString(al, false, true); // not reversed, top only
+         Uint jointFreq = p.getJointFreq();
+      */
 
          GreenReader('_').operator()(al, sets);
          if (sets.size() < stoks.size())
@@ -81,8 +133,11 @@ int main(int argc, char* argv[])
          if (lang == 1) {
             for (Uint i = 0; i < sets.size(); ++i)
                if (sets[i].empty() || sets[i][0] == ttoks.size()) {
-                  voc.add(stoks[i].c_str(), p.getJointFreq());
-                  if (verbose) writeContextPhrase(stoks[i], p, al);
+                  voc.add(stoks[i].c_str(), jointFreq);
+                  //if (verbose) writeContextPhrase(stoks[i], p, al);
+                  if (verbose)
+                     cerr << stoks[i] << " - unaligned in: " << join(stoks) << " ||| "
+                          << join(ttoks) << " ||| " << jointFreq << " " << al << endl;
                }
          } else {
             tpos.resize(ttoks.size(), false);
@@ -93,8 +148,11 @@ int main(int argc, char* argv[])
                   tpos[sets[i][j]] = true;
             for (Uint i = 0; i < ttoks.size(); ++i)
                if (!tpos[i]) {
-                  voc.add(ttoks[i].c_str(), p.getJointFreq());
-                  if (verbose) writeContextPhrase(ttoks[i], p, al);
+                  voc.add(ttoks[i].c_str(), jointFreq);
+                  //if (verbose) writeContextPhrase(ttoks[i], p, al);
+                  if (verbose)
+                     cerr << ttoks[i] << " - unaligned in: " << join(stoks) << " ||| "
+                          << join(ttoks) << " ||| " << jointFreq << " " << al << endl;
                }
          }
       }
