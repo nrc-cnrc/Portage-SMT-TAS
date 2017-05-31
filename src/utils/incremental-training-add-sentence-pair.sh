9e8c3a056ee80f2a91144528d396ed5bb5bee98d
Samuel Larkin <Samuel.Larkin@cnrc-nrc.gc.ca>
2017-05-31 10:18:32 -0400

    Bash variables should be lowercase.

--- src/utils/incremental-training-add-sentence-pair.sh
+++ src/utils/incremental-training-add-sentence-pair.sh
@@ -39,30 +39,30 @@
 # IMPORTANT NOTE:
 # We need a separate and empty file for the lock.  We cannot use `queue` as the
 # queue file AND the lock file.
-readonly CORPORA=corpora
-readonly QUEUE=queue
-readonly QUEUE_FD=200
-readonly QUEUE_LOCK=queue.lock
-readonly ISTRAINING_FD=201
-readonly ISTRAINING_LOCK=istraining.lock
+readonly corpora=corpora
+readonly queue=queue
+readonly queue_fd=200
+readonly queue_lock=queue.lock
+readonly istraining_fd=201
+readonly istraining_lock=istraining.lock
 
 # Includes NRC's bash library.
-BIN=`dirname $0`
-if [[ ! -r $BIN/sh_utils.sh ]]; then
+bin=`dirname $0`
+if [[ ! -r $bin/sh_utils.sh ]]; then
    # assume executing from src/* directory
-   #BIN="$BIN/../utils"
-   BIN="$BIN/utils"
+   #bin="$bin/../utils"
+   bin="$bin/utils"
 fi
-source $BIN/sh_utils.sh || { echo "Error: Unable to source sh_utils.sh" >&2; exit 1; }
+source $bin/sh_utils.sh || { echo "Error: Unable to source sh_utils.sh" >&2; exit 1; }
 
 function usage() {
    for msg in "$@"; do
       echo $msg >&2
    done
-   [[ $0 =~ [^/]*$ ]] && PROG=$BASH_REMATCH || PROG=$0
+   [[ $0 =~ [^/]*$ ]] && prog=$BASH_REMATCH || prog=$0
    cat <<==EOF== >&2
 
-Usage: $PROG [options] INCREMENTAL_SCRIPT  SOURCE TRANSLATION
+Usage: $prog [options] INCREMENTAL_SCRIPT  SOURCE TRANSLATION
 
   Brief description
 
@@ -83,11 +83,11 @@ Options:
 VERBOSE=0
 while [ $# -gt 0 ]; do
    case "$1" in
-   -extra-data)         arg_check 1 $# $1; readonly EXTRA_DATA=$2; shift;;
+   -extra-data)         arg_check 1 $# $1; readonly extra_data=$2; shift;;
 
    -v|-verbose)         VERBOSE=$(( $VERBOSE + 1 ));;
    -d|-debug)           DEBUG=1;;
-   -u|-unittest)        UNITTEST=1;;
+   -u|-unittest)        unittest=1;;
    -h|-help)            usage;;
    --)                  shift; break;;
    -*)                  error_exit "Unknown option $1.";;
@@ -96,14 +96,14 @@ while [ $# -gt 0 ]; do
    shift
 done
 
-readonly INCREMENTAL_TRAINING=$1
-readonly SOURCE_SENTENCE=`tr "\t" " " <<< $2`
-readonly TARGET_SENTENCE=`tr "\t" " " <<< $3`
+readonly incremental_training=$1
+readonly source_sentence=`tr "\t" " " <<< $2`
+readonly target_sentence=`tr "\t" " " <<< $3`
 
 # When running in unittest mode, it should automatically trigger verbose.
-[[ $UNITTEST ]] && VERBOSE=$(( $VERBOSE + 1 ))
+[[ $unittest ]] && VERBOSE=$(( $VERBOSE + 1 ))
 
-if [[ -z $INCREMENTAL_TRAINING ]]; then
+if [[ -z $incremental_training ]]; then
    ! echo "Error: You must provide an incremental training script." >&2
    exit 1
 fi
@@ -114,72 +114,72 @@ fi
 
 
 
-eval "exec $QUEUE_FD>$QUEUE_LOCK"
+eval "exec $queue_fd>$queue_lock"
 verbose 1 "Locking the queue"
-flock --exclusive $QUEUE_FD
+flock --exclusive $queue_fd
 
-if [[ -n "$SOURCE_SENTENCE" && -n "$TARGET_SENTENCE" ]]; then
-   verbose 1 "Adding to the queue: $SOURCE_SENTENCE $TARGET_SENTENCE"
-   [[ -z $UNITTEST ]] && usleep 300000  # Unittest delay
+if [[ -n "$source_sentence" && -n "$target_sentence" ]]; then
+   verbose 1 "Adding to the queue: $source_sentence $target_sentence"
+   [[ -z $unittest ]] && usleep 300000  # Unittest delay
    {
-      #echo -n $SOURCE_SENTENCE$'\t'$TARGET_SENTENCE;
-      echo -n $SOURCE_SENTENCE;
+      #echo -n $source_sentence$'\t'$target_sentence;
+      echo -n $source_sentence;
       echo -n $'\t';
-      echo -n $TARGET_SENTENCE;
-      [[ -z "$EXTRA_DATA" ]] || echo -ne "\t$EXTRA_DATA";
+      echo -n $target_sentence;
+      [[ -z "$extra_data" ]] || echo -ne "\t$extra_data";
       echo
-   } >> $QUEUE
+   } >> $queue
    if [[ $? -ne 0 ]];
    then
       echo "Error writing sentence pair to the queue" >&2
       verbose 1 "Unlocking isTraining"
-      flock --unlock $QUEUE_FD
+      flock --unlock $queue_fd
       exit 1
    fi
 fi
 
 {
-   eval "exec $ISTRAINING_FD>$ISTRAINING_LOCK"
+   eval "exec $istraining_fd>$istraining_lock"
    verbose 1 "Locking isTraining"
-   #flock --exclusive --nonblock $ISTRAINING_FD \
-   #|| { echo "Training is already in progress"; echo "Unlocking isTraining"; flock --unlock $ISTRAINING_FD; exit; }
-   #flock --exclusive --nonblock $ISTRAINING_FD \
+   #flock --exclusive --nonblock $istraining_fd \
+   #|| { echo "Training is already in progress"; echo "Unlocking isTraining"; flock --unlock $istraining_fd; exit; }
+   #flock --exclusive --nonblock $istraining_fd \
    #|| ! echo "Training is already in progress" \
    #|| ! echo "Unlocking isTraining" \
-   #|| ! flock --unlock $ISTRAINING_FD \
+   #|| ! flock --unlock $istraining_fd \
    #|| exit
-   flock --exclusive --nonblock $ISTRAINING_FD
+   flock --exclusive --nonblock $istraining_fd
    if [[ $? -eq 1 ]];
    then
       verbose 1 "Training is already in progress"
       verbose 1 "Unlocking isTraining"
-      flock --unlock $QUEUE_FD
+      flock --unlock $queue_fd
       exit 2  # At this point in time, there shouldn't be anyone to see this error code.
    fi
 
    # As long as there is something in the queue, let's train.
-   #while [[ `wc -l < $QUEUE` -gt 0 ]];
-   while [[ -s $QUEUE ]];
+   #while [[ `wc -l < $queue` -gt 0 ]];
+   while [[ -s $queue ]];
    do
       verbose 1 "Adding the queue to corpora"
-      cat $QUEUE >> $CORPORA
-      cat /dev/null > $QUEUE
+      cat $queue >> $corpora
+      cat /dev/null > $queue
       verbose 1 "Unlocking the queue"
-      flock --unlock $QUEUE_FD
+      flock --unlock $queue_fd
 
       # Train
       verbose 1 "$$ is training"
-      eval $INCREMENTAL_TRAINING $CORPORA &> incremental-update.log
+      eval $incremental_training $corpora &> incremental-update.log
 
-      eval "exec $QUEUE_FD>$QUEUE_LOCK"
+      eval "exec $queue_fd>$queue_lock"
       verbose 1 "Locking the queue"
-      flock --exclusive $QUEUE_FD
+      flock --exclusive $queue_fd
    done
 
    verbose 1 "Unlocking isTraining"
-   flock --unlock $ISTRAINING_FD
+   flock --unlock $istraining_fd
    verbose 1 "Unlocking the queue"
-   flock --unlock $QUEUE_FD
+   flock --unlock $queue_fd
 } &
 
 disown -h %1
