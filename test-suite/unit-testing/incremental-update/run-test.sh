11c9e9ea2d69489ac92e32ad332762b41447589d
Darlene Stewart <Darlene.Stewart@nrc-cnrc.gc.ca>
2017-05-31 17:23:12 -0400

    Improved timing and other output for incremental-update unit-tests.
    Also, test the exclusivity of the incremental-update.lock held by
    incrmental-update.sh.

--- test-suite/unit-testing/incremental-update/run-test.sh
+++ test-suite/unit-testing/incremental-update/run-test.sh
@@ -12,6 +12,12 @@
 # Copyright 2017, Sa Majeste la Reine du Chef du Canada
 # Copyright 2017, Her Majesty in Right of Canada
 
+timeFormat()
+{
+   local tag=$1
+   TIMEFORMAT=$'\n'"$tag:  "$'real %3lR\tuser %3lU\tsys %3lS'   
+}
+
 make clean
 make gitignore
 
@@ -30,13 +36,17 @@ utokenize.pl -noss -lang en < s1 | utf8_casemap -c l | canoe-escapes.pl > s1.rul
 
 # Create an empty incremental model, with only a dummy corpus
 echo `date +"%F %T"`'	__DUMMY__ __DUMMY__	__DUMMY__ __DUMMY__' > corpora
+timeFormat "incr-update DUMMY"
 time incremental-update.sh -f config -v en fr corpora >& log.update-dummy
+timeFormat "canoe DUMMY"
 time canoe -f canoe.ini.incr -input s1.rules > s1.out 2> log.s1.out
 
 # Create a model with the s1 t1 sentence pair update, and redecode to see
 # "Mr. Boazek" correctly translated to "M. Baozeck".
 paste <(echo `date +"%F %T"`) s1 t1 > corpora
+timeFormat "incr-update s1"
 time incremental-update.sh -f config -v en fr corpora >& log.update-s1t1
+timeFormat "canoe s1"
 time canoe -f canoe.ini.incr -input s1.rules > s1.out.incr 2> log.s1.out.incr
 grep -i Baozeck s1.out.incr
 
@@ -57,22 +67,36 @@ canoe-escapes.pl < dev1_en.tok > dev1_en.rules
    readonly UPDATE_LOCK_FD=202
    readonly UPDATE_LOCK_FILE=incremental-update.lock
    eval "exec $UPDATE_LOCK_FD>$UPDATE_LOCK_FILE"
-   flock --shared $UPDATE_LOCK_FD
-   time canoe -f canoe.ini.incr -input dev1_en.rules > dev1.out.0 2> log.dev1.out.0
-   sleep 5
-   flock --unlock $UPDATE_LOCK_FD
+   for n in 1 2; do
+      until flock --nonblock --shared $UPDATE_LOCK_FD; do
+         echo "canoe.0-$n failed to acquire incremental-update.lock... retrying in 1 second." >&2
+         sleep 1
+      done
+      echo "canoe.0-$n acquired incremental-update.lock" >&2
+      timeFormat canoe.0-$n
+      time canoe -f canoe.ini.incr -input dev1_en.rules > dev1.out.0-$n 2> log.dev1.out.0-$n
+      if [[ $n == 1 ]]; then
+         ln -sf dev1.out.0-1 dev1.out.0
+         echo "canoe.0-$n keeping incremental-update.lock for 5 more seconds." >&2
+         sleep 5
+      fi
+      echo "canoe.0-$n releasing incremental-update.lock" >&2
+      flock --unlock $UPDATE_LOCK_FD
+   done
 ) &
 
 # to speed up the unit test, updates are only actually done $UPDATE_FREQ lines at a time.
 UPDATE_FREQ=12
 cat < /dev/null > dev1.out.incr
 for l in $(seq 1 `wc -l < dev1_fr.raw`); do
+   timeFormat canoe.$l
    time select-line $l dev1_en.rules \
       | canoe -f canoe.ini.incr \
       >> dev1.out.incr 2> log.dev1.out.incr-$l
-   if (($l / $UPDATE_FREQ * $UPDATE_FREQ == $l)); then
+   if (($l % $UPDATE_FREQ == 0)); then
       echo Doing incremental update using $l lines
       paste <(yes `date +"%F %T"` | head -$l) <(head -$l dev1_en.raw) <(head -$l dev1_fr.raw) > corpora
+      timeFormat "incr-update $l"
       time incremental-update.sh -f config -v en fr corpora >& log.update-$l
    fi
    #time canoe -f canoe.ini.incr -input dev1_en.rules > dev1.out.$l 2> log.dev1.out.$l
