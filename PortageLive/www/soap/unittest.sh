15aa45469d9946f400c56d79470b6b5173f7f6c2
Samuel Larkin <Samuel.Larkin@cnrc-nrc.gc.ca>
2017-06-12 11:27:21 -0400

    Change the token to the server's ip when deploying.

--- PortageLive/www/soap/unittest.sh
+++ PortageLive/www/soap/unittest.sh
@@ -1,6 +1,13 @@
 #!/bin/bash
 
-[[ $DEPLOY ]] && cp PortageLiveAPI.wsdl ../PortageLiveLib.php PortageLiveAPI.php /var/www/html/
+if [[ $DEPLOY ]];
+then
+   cp ../PortageLiveLib.php PortageLiveAPI.php /var/www/html/
+   sed \
+      "s/__REPLACE_THIS_WITH_YOUR_IP__/`ifconfig | grep -A8 eth | grep -Po 'addr:\K[\d.]+'`/g" \
+      < PortageLiveAPI.wsdl \
+      > /var/www/html/PortageLiveAPI.wsdl
+fi
 
 # Web Unittest that use a functional web service.
 python -m unittest discover -s tests -p 'test*.py'
