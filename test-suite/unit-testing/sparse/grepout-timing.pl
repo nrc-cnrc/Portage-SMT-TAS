3cb2a54a554598e5b753af8bf53119f4478ac7f7
Eric Joanis <Eric.Joanis@cnrc-nrc.gc.ca>
2018-01-10 16:56:22 -0500

    Adjusted sparse test suite to current canoe output.

--- test-suite/unit-testing/sparse/grepout-timing.pl
+++ test-suite/unit-testing/sparse/grepout-timing.pl
@@ -6,7 +6,7 @@ while (<STDIN>) {
   s/^Done in [0-9]+s//go;
   s/(\.\.\. kept: [0-9]+ in )[0-9]+s./$1/go;
   s/^LM loading completed.+//go;
-  s/^Loaded data structures.+//go;
+  s/^Loaded .+//go;
   s/^Translated 10 sentences.+//go;
   s/^(createModel|runDecoder|doOutput).+//go;
   s/^canoe done on.+//go;
