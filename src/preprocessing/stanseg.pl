f6dc52c43a8c3f0723a8a21b4557f7fc683f5ce7
Eric Joanis <Eric.Joanis@cnrc-nrc.gc.ca>
2019-01-29 17:04:19 -0500

    Bug fix: stanseg.pl should cleanly handle empty block back from segmenter

--- src/preprocessing/stanseg.pl
+++ src/preprocessing/stanseg.pl
@@ -188,14 +188,15 @@ binmode STAN_SEG_PIPE, ":encoding(UTF-8)";
 ### Phase 4: merge the processed Arabic and non-Arabic streams
 
 while (<NON_AR_IN>) {
-   s|__ARABIC__ID([0-9]+)__|
+   s&__ARABIC__ID([0-9]+)__&
       my $contents = <STAN_SEG_PIPE>;
+      $debug and print STDERR "FROM STANSEG: $contents";
       die "Error: Missing line in $ar_filename for ARABIC ID$1" if (!defined $contents);
       chomp $contents;
       $contents =~ s/^BEGIN +//;
-      $contents =~ s/ +END$//;
+      $contents =~ s/(^| +)END$//;
       $contents
-   |eg;
+   &eg;
    s/__ESCAPE_ARABIC_ID/__ARABIC__ID/g; # Undo collision avoidance
    s/   */ /g;
    s/ *$//;
