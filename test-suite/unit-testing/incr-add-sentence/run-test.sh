01cb790973b7b47b032fdbf309401dbb1b671f9c
Eric Joanis <Eric.Joanis@cnrc-nrc.gc.ca>
2019-04-18 13:39:58 -0400

    better catch and report errors in incr-add-sentence test suite

--- test-suite/unit-testing/incr-add-sentence/run-test.sh
+++ test-suite/unit-testing/incr-add-sentence/run-test.sh
@@ -18,9 +18,6 @@ readonly Red='\e[0;31m'
 readonly Bold='\e[1m'
 readonly Reset='\e[00m'
 
-trap "trap - EXIT; echo Test FAILED" ERR
-trap "echo All tests PASSED." EXIT
-
 
 readonly CORPORA=corpora
 readonly QUEUE=queue
@@ -43,14 +40,15 @@ function cleanup() {
 
 
 
-function testcaseDescritption() {
+function testcaseDescription() {
    echo -e "${Bold}Testcase:${Reset} $@" >&2
 }
 
 
-
+GOT_ERROR=0
 function error_message() {
    echo -e "${BoldRed}Error: ${Red}$@${Reset}" >&2
+   GOT_ERROR=1
 }
 
 
@@ -75,8 +73,7 @@ function trigger_single_training() {
 
 
 function with_extra_data() {
-   set -o errexit
-   testcaseDescritption "Adding extra data"
+   testcaseDescription "Adding extra data"
    cleanup
    # We want to use a stub for incr-update.sh.
    PATH=.:$PATH \
@@ -85,31 +82,31 @@ function with_extra_data() {
       -unittest \
       -extra-data '{"a": 1}' \
       "source_extra" \
-      "translation_extra"
+      "translation_extra" \
+   || error_message "Error calling incr-add-sentence.sh."
    sleep $((TRAINING_TIME+1))
 
    cut -f 1 $CORPORA \
    | grep --quiet `date +"%F"` \
-   || ! error_message "Can't find the date."
+   || error_message "Can't find the date."
 
    cut -f 2 $CORPORA \
    | grep --quiet 'source_extra' \
-   || ! error_message "Can't find the source."
+   || error_message "Can't find the source."
 
    cut -f 3 $CORPORA \
    | grep --quiet 'translation_extra' \
-   || ! error_message "Can't find the translation."
+   || error_message "Can't find the translation."
 
    cut -f 4 $CORPORA \
    | grep --quiet '{"a": 1}' \
-   || ! error_message "Can't find the extra data."
+   || error_message "Can't find the extra data."
 }
 
 
 
 function with_utf8() {
-   set -o errexit
-   testcaseDescritption "Let's use UTF-8"
+   testcaseDescription "Let's use UTF-8"
    cleanup
    # We want to use a stub for incr-update.sh.
    PATH=.:$PATH \
@@ -117,17 +114,17 @@ function with_utf8() {
       -verbose \
       -unittest \
       "source_utf8_É" \
-      "⅀translation_utf8_¿"
+      "⅀translation_utf8_¿" \
+   || error_message "Error calling incr-add-sentence.sh."
    sleep $((TRAINING_TIME+1))
    grep --quiet $'source_utf8_É\t⅀translation_utf8_¿' $CORPORA \
-   || ! error_message "Can't UTF-8 characters."
+   || error_message "Can't UTF-8 characters."
 }
 
 
 
 function with_quotes() {
-   set -o errexit
-   testcaseDescritption "Let's include quotes and stuff"
+   testcaseDescription "Let's include quotes and stuff"
    cleanup
    # We want to use a stub for incr-update.sh.
    PATH=.:$PATH \
@@ -135,17 +132,17 @@ function with_quotes() {
       -verbose \
       -unittest \
       "double\"single'backtick\`left<right>quotes" \
-      "double\"single'backtick\`left<right>quotes"
+      "double\"single'backtick\`left<right>quotes" \
+   || error_message "Error calling incr-add-sentence.sh."
    sleep $((TRAINING_TIME+1))
    grep --quiet "double\"single'backtick\`left<right>quotes	double\"single'backtick\`left<right>quotes" $CORPORA \
-   || ! error_message "Quotes got garbled."
+   || error_message "Quotes got garbled."
 }
 
 
 
 function with_tab() {
-   set -o errexit
-   testcaseDescritption "Let's include litteral tabs"
+   testcaseDescription "Let's include litteral tabs"
    cleanup
    # We want to use a stub for incr-update.sh.
    PATH=.:$PATH \
@@ -153,17 +150,17 @@ function with_tab() {
       -verbose \
       -unittest \
       $'source\tsource\tsource' \
-      "target\ttarget\ttarget"
+      "target\ttarget\ttarget" \
+   || error_message "Error calling incr-add-sentence.sh."
    sleep $((TRAINING_TIME+1))
    grep --quiet $'source source source\ttarget\\\\ttarget\\\\ttarget' $CORPORA \
-   || ! error_message "We can't find TABs."
+   || error_message "We can't find TABs."
 }
 
 
 
 function unable_to_write_to_the_queue(){
-   set -o errexit
-   testcaseDescritption "Unable to write to the queue"
+   testcaseDescription "Unable to write to the queue"
    cleanup
    touch $QUEUE
    chmod a-w $QUEUE
@@ -175,13 +172,13 @@ function unable_to_write_to_the_queue(){
       "S-$$ `date +'%T:%N'`" \
       "T-$$ `date +'%T:%N'`" \
    |& grep --quiet 'Error writing sentence pair to the queue' \
-   || ! error_message "We shouldn't be able to write to the queue."
+   || error_message "We shouldn't be able to write to the queue."
 }
 
 
 
 function multiple_add_and_multiple_trigger() {
-   testcaseDescritption "Multiple add and multiple training triggers."
+   testcaseDescription "Multiple add and multiple training triggers."
    cleanup
 
    # Let's prepopulate the queue with at least one sentence pair and then let
@@ -202,7 +199,8 @@ function multiple_add_and_multiple_trigger() {
             -unittest \
             "$i-S-$$ `date +'%T:%N'`" \
             "$i-T-$$ `date +'%T:%N'`" \
-            &> $LOG.add.$i;
+            &> $LOG.add.$i \
+            || error_message "Error calling incr-add-sentence.sh."
       } &
       # Trigger training only.
       {
@@ -212,7 +210,8 @@ function multiple_add_and_multiple_trigger() {
          incr-add-sentence.sh \
             -verbose \
             -unittest \
-            &> $LOG.trigger.$i;
+            &> $LOG.trigger.$i \
+            || error_message "Error calling incr-add-sentence.sh."
       } &
    done
 
@@ -232,45 +231,44 @@ function multiple_add_and_multiple_trigger() {
    done
 
    echo "Validating..." >&2
-   set -o errexit
 
    [[ `find -name $LOG.add.\* | wc -l` -eq $MAX_CONCURRENT_CALLS ]] \
-   || ! error_message "We should have got $MAX_CONCURRENT_CALLS log.add files."
+   || error_message "We should have got $MAX_CONCURRENT_CALLS log.add files."
 
    [[ `find -name $LOG.trigger.\* | wc -l` -eq $MAX_CONCURRENT_CALLS ]] \
-   || ! error_message "We should have got $MAX_CONCURRENT_CALLS log.trigger files."
+   || error_message "We should have got $MAX_CONCURRENT_CALLS log.trigger files."
 
    [[ `wc -l < $CORPORA` -eq $(($MAX_CONCURRENT_CALLS + 1)) ]] \
-   || ! error_message "We should have got $(($MAX_CONCURRENT_CALLS + 1)) sentence pairs in the corpus."
+   || error_message "We should have got $(($MAX_CONCURRENT_CALLS + 1)) sentence pairs in the corpus."
 
    # Let's make sure the sentence pairs have the format that we expect.
    [[ `grep --count --perl-regexp '^[0-9: -]+\t\d{1,}-S-\d{1,7} \d{2}:\d{2}:\d{2}:\d{1,9}\t\d{1,}-T-\d{1,7} \d{2}:\d{2}:\d{2}:\d{1,9}' $CORPORA` -eq $MAX_CONCURRENT_CALLS ]] \
-   || ! error_message "Some sentence pairs are corrupted."
+   || error_message "Some sentence pairs are corrupted."
 
    [[ `wc -l < $QUEUE` -eq 0 ]] \
-   || ! error_message "All elements of the queue should have been procesed."
+   || error_message "All elements of the queue should have been procesed."
 
    # All except one process should claim that training is already ongoing.
    [[ `grep 'Training is already in progress' $LOG.* | wc -l` -eq $((2 * $MAX_CONCURRENT_CALLS - 1)) ]] \
-   || ! error_message "All except one process should claim that training is already ongoing."
+   || error_message "All except one process should claim that training is already ongoing."
 
    # We expect the first process to add to queue will start training then, the
    # same process will have to redo training since the other 9 processes will have
    # added their sentence pair.
    [[ `grep --files-with-matches 'is training' $LOG.* | wc -l` -eq 1 ]] \
-   || ! error_message "Only one training should be ongoing at once."
+   || error_message "Only one training should be ongoing at once."
 
    [[ `grep --count 'is training' $LOG.* | grep --count --invert-match ':0$'` -eq 1 ]] \
-   || ! error_message "Only one process should have trained multiple times."
+   || error_message "Only one process should have trained multiple times."
 
    [[ `wc -l < $WITNESS` -eq 2 ]] \
-   || ! error_message "The witness should report 2."
+   || error_message "The witness should report 2."
 }
 
 
 
 function insert_and_multiple_trigger_training() {
-   testcaseDescritption "Multiple simultaneous triggers for training."
+   testcaseDescription "Multiple simultaneous triggers for training."
    cleanup
    add_sentence_pair_to_queue
 
@@ -285,7 +283,8 @@ function insert_and_multiple_trigger_training() {
          incr-add-sentence.sh \
             -verbose \
             -unittest \
-            &> $LOG.trigger.$i;
+            &> $LOG.trigger.$i \
+            || error_message "Error calling incr-add-sentence.sh."
       } &
    done
 
@@ -303,17 +302,16 @@ function insert_and_multiple_trigger_training() {
    done
 
    echo "Validating..." >&2
-   set -o errexit
 
    [[ `grep --files-with-matches 'is training' $LOG.trigger.* | wc -l` -eq 1 ]] \
-   || ! error_message "Only one training should be ongoing at once."
+   || error_message "Only one training should be ongoing at once."
 
    # All except one process should claim that training is already ongoing.
    [[ `grep 'Training is already in progress' $LOG.trigger.* | wc -l` -eq $(($MAX_CONCURRENT_CALLS - 1)) ]] \
-   || ! error_message "Only one process should have trained multiple times."
+   || error_message "Only one process should have trained multiple times."
 
    [[ `wc -l < $WITNESS` -eq 1 ]] \
-   || ! error_message "The witness should report 1."
+   || error_message "The witness should report 1."
 }
 
 
@@ -327,28 +325,29 @@ function integration_with_incremental_update_sh() {
    rm -fr incremental-tmp.???
    incr-add-sentence.sh \
       "Il faut aller à l'école." \
-      'We must go to school.'
+      'We must go to school.' \
+      || error_message "Error calling incr-add-sentence.sh."
 
    sleep 1
 
    # We should find a single entry in the corpora.
    [[ `wc -l < $CORPORA` -eq 1 ]] \
-   || ! error_message "We should find a single entry in the corpora."
+   || error_message "We should find a single entry in the corpora."
 
    [[ `cut -f 1 $CORPORA` =~ `date +%F` ]] \
-   || ! error_message "Couldn't find the proper source sentence."
+   || error_message "Couldn't find the proper source sentence."
 
    [[ `cut -f 2 $CORPORA` == "Il faut aller à l'école." ]] \
-   || ! error_message "Couldn't find the proper source sentence."
+   || error_message "Couldn't find the proper source sentence."
 
    [[ `cut -f 3 $CORPORA` == 'We must go to school.' ]] \
-   || ! error_message "Couldn't find the proper translation sentence."
+   || error_message "Couldn't find the proper translation sentence."
 }
 
 
 
 which incr-add-sentence.sh &> /dev/null \
-|| { ! error_message "incr-add-sentence.sh not defined"; exit 1; }
+|| { error_message "incr-add-sentence.sh not defined"; exit 1; }
 
 unable_to_write_to_the_queue
 with_extra_data
@@ -359,3 +358,9 @@ multiple_add_and_multiple_trigger
 insert_and_multiple_trigger_training
 integration_with_incremental_update_sh
 
+if [[ $GOT_ERROR -eq 1 ]]; then
+   echo Test FAILED.
+else
+   echo All tests PASSED.
+fi
+exit $GOT_ERROR
