150d095683af7072bb0881f18db9cf84776006e8
Eric Joanis <Eric.Joanis@cnrc-nrc.gc.ca>
2019-04-18 15:46:56 -0400

    madamira.py unit test: create the .gitignore file

--- test-suite/unit-testing/madamira.py/run-test.sh
+++ test-suite/unit-testing/madamira.py/run-test.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+make gitignore
+
 if [[ $PBS_JOBID || $GECOSHEP_JOB_ID ]]; then
    run-parallel.sh -v -psub "-mem 16" -c ./madamira-test.sh
    run-parallel.sh -v -psub "-mem 16" -c ./stanseg-test.sh
