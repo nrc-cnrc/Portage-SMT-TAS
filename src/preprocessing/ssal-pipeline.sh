a937c4ac99e6f9b7e2c7a008be1ee453df40064c
Eric Joanis <Eric.Joanis@cnrc-nrc.gc.ca>
2020-01-27 11:47:54 -0500

    complete the features needed in ssal-pipeline.sh
    (cherry picked from Portage R&D commit ee3b6ca91e018735684fd924921dbb4ae57c0e1b)

--- src/preprocessing/ssal-pipeline.sh
+++ src/preprocessing/ssal-pipeline.sh
@@ -26,16 +26,16 @@ usage() {
    [[ $0 =~ [^/]*$ ]] && PROG=$BASH_REMATCH || PROG=$0
    cat <<==EOF== >&2
 
-Usage: $PROG [-c CONFIG] [options] L1_FILE L2_FILE
+Usage: $PROG [-c CONFIG] [options] L1_FILE_IN L2_FILE_IN L1_FILE_OUT L2_FILE_OUT
 
-  Run a full sentence alignment pipeline on L1_FILE and L2_FILE:
+  Run a full sentence alignment pipeline on L[12]_FILE_IN:
    1. Clean the corpus files
    2. Align paragraphs, using the IBM model provided
    3. Sentence split
    4. Tokenize
    5. Align sentences within paragraphs, using the IBM model provided
 
-  The output will be cleaned, non-tokenized sentences.
+  The output will be cleaned, non-tokenized sentences, saved into L[12]_FILE_OUT
 
   Config file CONFIG should contain the configuration settings parameters for
   the language pair, as bash variable definitions.
@@ -45,8 +45,6 @@ Usage: $PROG [-c CONFIG] [options] L1_FILE L2_FILE
      L1_TOK, L2_TOK: commands to tokenized and maybe lowercase for the IBM model
      IBM_L1_GIVEN_L2, IBM_L2_GIVEN_L1: IBM models
   E.g.:
-     L1=en
-     L2=iu
      L1_CLEAN="clean-utf8-text.pl"
      L2_CLEAN="clean-utf8-text.pl | normalize-iu-spelling.pl"
      L1_SS="utokenize.pl -lang=en -notok -ss -paraline -p"
@@ -61,9 +59,12 @@ Usage: $PROG [-c CONFIG] [options] L1_FILE L2_FILE
 
 Options:
 
-  -c CONFIG    Config file to read
-  -n(otreally) Show what will happen but don't run anything
-  -header N    Assume the first N lines of the files are fixed headers.
+  -c CONFIG    Config file to read (required)
+  -n(otreally) Show what will happen but don't run anything [do the work]
+  -header N    Assume the first N lines of the files are fixed headers [none]
+  -ibm         Use IBM models [automatic if IBM_L1_GIVEN_L2 and IBM_L2_GIVEN_L1 are defined]
+  -no-ibm      Don't use IBM models
+  -f(orce)     Overwrite existing output files [don't]
   -h(elp)      print this help message
   -v(erbose)   increment the verbosity level by 1 (may be repeated)
   -d(ebug)     print debugging information
@@ -74,12 +75,20 @@ Options:
 }
 
 VERBOSE=0
-CONFIG_FILE=""
+CONFIG_FILE=
+NOTREALLY=
+FORCE=
+HEADER=
+DEBUG=
+USE_IBM=
 while [[ $# -gt 0 ]]; do
    case "$1" in
    -c)                  arg_check 1 $# $1; CONFIG_FILE=$2; shift;;
    -n|-notreally)       NOTREALLY=1;;
    -header)             arg_check 1 $# $1; HEADER=$2; shift;;
+   -ibm)                USE_IBM=1;;
+   -no-ibm)             USE_IBM=0;;
+   -f|-force)           FORCE=1;;
    -v|-verbose)         VERBOSE=$(( $VERBOSE + 1 ));;
    -d|-debug)           DEBUG=1;;
    -h|-help)            usage;;
@@ -92,11 +101,17 @@ done
 
 [[ $CONFIG_FILE ]] || error_exit "-c CONFIG argument is required."
 [[ -r $CONFIG_FILE ]] || error_exit "Cannot read config file $CONFIG_FILE."
-[[ $# -eq 2 ]] || error_exit "Exactly two arguments are required: L1_FILE and L2_FILE."
-L1_FILE=$1
-L2_FILE=$2
+[[ $# -eq 4 ]] || usage "Exactly four arguments are required: L1_FILE_IN, L2_FILE_IN, L1_FILE_OUT, L2_FILE_OUT."
+L1_FILE_IN=$1
+L2_FILE_IN=$2
+L1_FILE_OUT=$3
+L2_FILE_OUT=$4
 
 source $CONFIG_FILE || or error_exit "Error reading config file $CONFIG_FILE."
+
+if [[ ! $USE_IBM && $IBM_L2_GIVEN_L1 && $IBM_L1_GIVEN_L2 ]]; then
+   USE_IBM=1
+fi
 
 WD=`mktemp -d ssal-pipeline.tmp.XXXX` || error_exit "Cannot create temporary working directory."
 [[ $VERBOSE > 0 ]] && echo "Working Directory $WD"
@@ -114,8 +129,8 @@ r() {
 
 
 # Step 1: clean the input
-r "cat $L1_FILE | $L1_CLEAN > $WD/l1.clean"
-r "cat $L2_FILE | $L2_CLEAN > $WD/l2.clean"
+r "cat $L1_FILE_IN | $L1_CLEAN > $WD/l1.clean"
+r "cat $L2_FILE_IN | $L2_CLEAN > $WD/l2.clean"
 
 # Remove the header if requested
 if [[ $HEADER ]]; then
@@ -130,7 +145,11 @@ fi
 r "cat $WD/l1.$CLEAN_IN | $L1_TOK > $WD/l1.p.tok"
 r "cat $WD/l2.$CLEAN_IN | $L2_TOK > $WD/l2.p.tok"
 
-r "ssal -ibm_1g2 $IBM_L1_GIVEN_L2 -ibm_2g1 $IBM_L2_GIVEN_L1 -a $WD/p.scores $WD/l1.p.tok $WD/l2.p.tok"
+if [[ $USE_IBM == 1 ]]; then
+   r "ssal -ibm_1g2 $IBM_L1_GIVEN_L2 -ibm_2g1 $IBM_L2_GIVEN_L1 -a $WD/p.scores $WD/l1.p.tok $WD/l2.p.tok"
+else
+   r "ssal -a $WD/p.scores $WD/l1.p.tok $WD/l2.p.tok"
+fi
 
 r "select-lines.py -a 1 --joiner=$'\\n' --separator=$'\\n__PARAGRAPH__\\n' $WD/p.scores $WD/l1.$CLEAN_IN > $WD/l1.p.al"
 r "select-lines.py -a 2 --joiner=$'\\n' --separator=$'\\n__PARAGRAPH__\\n' $WD/p.scores $WD/l2.$CLEAN_IN > $WD/l2.p.al"
@@ -144,9 +163,28 @@ L2_PARAGRAPH_STR=$(echo __PARAGRAPH__ | eval $L2_TOK)
 r "cat $WD/l1.s | $L1_TOK | sed 's/$L1_PARAGRAPH_STR/__PARAGRAPH__/' > $WD/l1.s.tok"
 r "cat $WD/l2.s | $L2_TOK | sed 's/$L2_PARAGRAPH_STR/__PARAGRAPH__/' > $WD/l2.s.tok"
 
-r "ssal -hm __PARAGRAPH__ -fm -ibm_1g2 $IBM_L1_GIVEN_L2 -ibm_2g1 $IBM_L2_GIVEN_L1 -a $WD/s.scores $WD/l1.s.tok $WD/l2.s.tok"
+if [[ $USE_IBM == 1 ]]; then
+   r "ssal -hm __PARAGRAPH__ -fm -ibm_1g2 $IBM_L1_GIVEN_L2 -ibm_2g1 $IBM_L2_GIVEN_L1 -a $WD/s.scores $WD/l1.s.tok $WD/l2.s.tok"
+else
+   r "ssal -hm __PARAGRAPH__ -fm -a $WD/s.scores $WD/l1.s.tok $WD/l2.s.tok"
+fi
 
-r "select-lines.py -a 1 $WD/s.scores $WD/l1.s | sed -e 's/^ *//' -e 's/ *\$//' > $WD/l1.s.al"
-r "select-lines.py -a 2 $WD/s.scores $WD/l2.s | sed -e 's/^ *//' -e 's/ *\$//' > $WD/l2.s.al"
+# And append the aligned sentence pairs
+r "select-lines.py -a 1 $WD/s.scores $WD/l1.s | sed -e 's/^ *//' -e 's/ *\$//' >> $WD/l1.s.al"
+r "select-lines.py -a 2 $WD/s.scores $WD/l2.s | sed -e 's/^ *//' -e 's/ *\$//' >> $WD/l2.s.al"
+
+# Put the header back in, if required
+if [[ $HEADER ]]; then
+   r "head -$HEADER $WD/l1.clean > $L1_FILE_OUT"
+   r "head -$HEADER $WD/l2.clean > $L2_FILE_OUT"
+else
+   r "cat /dev/null > $L1_FILE_OUT"
+   r "cat /dev/null > $L2_FILE_OUT"
+fi
+
+# Cleanup: collapse sequences of more than one blank lines to just one
+r "paste $WD/l1.s.al $WD/l2.s.al | perl -nle '\$blank = /^\t\$/; print unless (\$blank and \$prevblank); \$prevblank = \$blank;' > $WD/pasted.s.al"
+r "cut -f 1 $WD/pasted.s.al >> $L1_FILE_OUT"
+r "cut -f 2 $WD/pasted.s.al >> $L2_FILE_OUT"
 
 [[ $DEBUG ]] || rm -rf $WD
